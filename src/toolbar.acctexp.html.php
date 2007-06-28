b200480dc347c33d6ee94314ffaac2444595c42e
mic <mic@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2007-06-28 13:43:19 +0000

    "new icon for central"
    git-svn-id: http://joomlacode.org/svn/aec/trunk@97 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/toolbar.acctexp.html.php
+++ src/toolbar.acctexp.html.php
@@ -1,18 +1,13 @@
-<?php /* $Id: toolbar.acctexp.html.php,v 1.2 2005/12/15 16:20:38 helder Exp $ */
+<?php
 /**
- * AcctExp Menubar HTML Writer
- * @package AcctExp
-* @copyright 2004-2007 Helder Garcia, David Deutsch
-* @license http://www.gnu.org/copyleft/gpl.html. GNU Public License
-* @version $Revision: 1.2 $
-* @author Helder Garcia <helder.garcia@gmail.com>, David Deutsch <skore@skore.de>
-**/
-//
-// Copyright (C) 2004-2007 Helder Garcia, David Deutsch
-// All rights reserved.
-// This source file is part of the Account Expiration Control Component, a  Joomla
-// custom Component By Helder Garcia and David Deutsch - http://www.globalnerd.org
-//
+ * @version $Id: toolbar.acctexp.html.php 16 2007-06-27 09:04:04Z mic $
+ * @package AEC - Account Control Expiration - Subscription component for Joomla! OS CMS
+ * @subpackage Menubar HTML Writer
+ * @copyright Copyright (C) 2004-2007, All Rights Reserved, Helder Garcia, David Deutsch
+ * @author Helder Garcia <helder.garcia@gmail.com>, David Deutsch <skore@skore.de> & Team AEC - http://www.gobalnerd.org
+ * @license GNU/GPL v.2 http://www.gnu.org/copyleft/gpl.html
+ */
+
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License (GPL)
 // as published by the Free Software Foundation; either version 2
@@ -31,10 +26,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//
-// The "GNU General Public License" (GPL) is available at
-// http://www.gnu.org/copyleft/gpl.html.
-//
+
 // Dont allow direct linking
 defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
 
@@ -44,20 +36,16 @@ class ComponentMenu {
 	*/
 	function NEW_MENU () {  // To be deleted?
 		mosMenuBar::startTable();
-		mosMenuBar::custom();
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'save', 'save.png',  'save_f2.png', _SAVE, false );
 		mosMenuBar::custom( 'cancel', 'cancel.png',  'cancel_f2.png', _CANCEL, false );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
 	function NEW_EXPIRATION () {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'add', 'edit.png',  'edit_f2.png', _CONFIGURE );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
@@ -65,8 +53,7 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'edit', 'edit.png',  'edit_f2.png', _CONFIGURE );
 		mosMenuBar::custom( 'expire', 'restore.png',  'restore_f2.png', _REMOVE );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
@@ -74,7 +61,6 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'save', 'save.png',  'save_f2.png', _SAVE, false );
 		mosMenuBar::custom( 'cancel', 'cancel.png',  'cancel_f2.png', _CANCEL, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -82,7 +68,6 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'save', 'save.png',  'save_f2.png', _SAVE, false );
 		mosMenuBar::custom( 'cancel', 'cancel.png',  'cancel_f2.png', _CANCEL, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -90,7 +75,6 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'saveSettings', 'save.png',  'save_f2.png', _SAVE, false );
 		mosMenuBar::custom( 'cancelSettings', 'cancel.png',  'cancel_f2.png', _CANCEL, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -98,21 +82,17 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'publishSubscriptionPlan', 'publish.png',  'publish_f2.png', _PUBLISH_PAYPLAN, true );
 		mosMenuBar::custom( 'unpublishSubscriptionPlan', 'unpublish.png',  'unpublish_f2.png', _UNPUBLISH_PAYPLAN, true );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'newSubscriptionPlan', 'new.png',  'new_f2.png', _NEW_PAYPLAN, false );
 		mosMenuBar::custom( 'editSubscriptionPlan', 'edit.png',  'edit_f2.png', _EDIT_PAYPLAN, true );
 		mosMenuBar::custom( 'removeSubscriptionPlan', 'delete.png',  'delete_f2.png', _REMOVE_PAYPLAN, true );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
 	function EDIT_SUBSCRIPTIONPLAN () {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'saveSubscriptionPlan', 'save.png',  'save_f2.png', _SAVE_PAYPLAN, false );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'cancelSubscriptionPlan', 'cancel.png',  'cancel_f2.png', _CANCEL_PAYPLAN, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -120,21 +100,17 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'publishMicroIntegration', 'publish.png',  'publish_f2.png', _PUBLISH_PAYPLAN, true );
 		mosMenuBar::custom( 'unpublishMicroIntegration', 'unpublish.png',  'unpublish_f2.png', _UNPUBLISH_PAYPLAN, true );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'newMicroIntegration', 'new.png',  'new_f2.png', _NEW_PAYPLAN, false );
 		mosMenuBar::custom( 'editMicroIntegration', 'edit.png',  'edit_f2.png', _EDIT_PAYPLAN, true );
 		mosMenuBar::custom( 'removeMicroIntegration', 'delete.png',  'delete_f2.png', _REMOVE_PAYPLAN, true );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
 	function EDIT_MICROINTEGRATION () {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'saveMicroIntegration', 'save.png',  'save_f2.png', _SAVE_PAYPLAN, false );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'cancelMicroIntegration', 'cancel.png',  'cancel_f2.png', _CANCEL_PAYPLAN, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -142,21 +118,17 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'publishCoupon', 'publish.png',  'publish_f2.png', _PUBLISH_PAYPLAN, true );
 		mosMenuBar::custom( 'unpublishCoupon', 'unpublish.png',  'unpublish_f2.png', _UNPUBLISH_PAYPLAN, true );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'newCoupon', 'new.png',  'new_f2.png', _NEW_PAYPLAN, false );
 		mosMenuBar::custom( 'editCoupon', 'edit.png',  'edit_f2.png', _EDIT_PAYPLAN, true );
 		mosMenuBar::custom( 'removeCoupon', 'delete.png',  'delete_f2.png', _REMOVE_PAYPLAN, true );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
 	function EDIT_COUPON () {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'saveCoupon', 'save.png',  'save_f2.png', _SAVE_PAYPLAN, false );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'cancelCoupon', 'cancel.png',  'cancel_f2.png', _CANCEL_PAYPLAN, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
@@ -164,21 +136,17 @@ class ComponentMenu {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'publishCouponStatic', 'publish.png',  'publish_f2.png', _PUBLISH_PAYPLAN, true );
 		mosMenuBar::custom( 'unpublishCouponStatic', 'unpublish.png',  'unpublish_f2.png', _UNPUBLISH_PAYPLAN, true );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'newCouponStatic', 'new.png',  'new_f2.png', _NEW_PAYPLAN, false );
 		mosMenuBar::custom( 'editCouponStatic', 'edit.png',  'edit_f2.png', _EDIT_PAYPLAN, true );
 		mosMenuBar::custom( 'removeCouponStatic', 'delete.png',  'delete_f2.png', _REMOVE_PAYPLAN, true );
-		mosMenuBar::divider();
-		mosMenuBar::custom( 'showCentral', 'back_f2.png',  'back_f2.png', _CENTRAL_PAGE , false );
+		mosMenuBar::custom( 'showCentral', 'extensions.png',  'extensions_f2.png', _CENTRAL_PAGE , false );
 		mosMenuBar::endTable();
 	}
 
 	function EDIT_COUPON_STATIC () {
 		mosMenuBar::startTable();
 		mosMenuBar::custom( 'saveCouponStatic', 'save.png',  'save_f2.png', _SAVE_PAYPLAN, false );
-		mosMenuBar::divider();
 		mosMenuBar::custom( 'cancelCouponStatic', 'cancel.png',  'cancel_f2.png', _CANCEL_PAYPLAN, false );
-		mosMenuBar::spacer();
 		mosMenuBar::endTable();
 	}
 
