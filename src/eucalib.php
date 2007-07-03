99beb90850881f049b2f7c825ffc8befbbb5bc29
mic <mic@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2007-07-03 13:25:53 +0000

    bugfix: syntax error, unexpected '{'
    git-svn-id: http://joomlacode.org/svn/aec/trunk@131 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/eucalib.php
+++ src/eucalib.php
@@ -6,35 +6,11 @@
  * @copyright Copyright (C) 2007 David Deutsch, All Rights Reserved
  * @author David Deutsch <skore@skore.de>
  * @license GNU/GPL v.2 http://www.gnu.org/copyleft/gpl.html
+ *
+ * The Extremely Useful Component LIBrary will rock your socks. Seriously. Reuse it!
  */
-// Copyright (C) 2006-2007 David Deutsch
-// All rights reserved.
-// This source file an abstract Library for Joomla! Components,
-// originally developed for the Account Expiration Control Component
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// Please note that the GPL states that any headers in files and
-// Copyright notices as well as credits in headers, source files
-// and output (screens, prints, etc.) can not be removed.
-// You can extend them with your own credits, though...
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-//
-// The "GNU General Public License" (GPL) is available at
-// http://www.gnu.org/copyleft/gpl.html.
 
-// The Extremely Useful Component LIBrary will rock your socks. Seriously. Reuse it!
+defined( '_VALID_MOS' ) or die( 'Restricted access' );
 
 /**
 * parameterized Database Table entry
@@ -92,7 +68,7 @@ class paramDBTable extends mosDBTable {
 			if( $overwrite ) {
 				$params[$key] = $value;
 			}else{
-				if( !isset( $params[$key] ){
+				if( !isset( $params[$key] ) ) {
 					$params[$key] = $value;
 				}
 			}
