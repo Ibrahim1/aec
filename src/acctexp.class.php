e925e3fa07de720a69836dc4ddab89497f15031d
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2007-07-03 18:10:40 +0000

    backend links should not go to a blank window ;)
    git-svn-id: http://joomlacode.org/svn/aec/trunk@146 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/acctexp.class.php
+++ src/acctexp.class.php
@@ -3793,7 +3793,7 @@ class AECToolbox {
 	function backendTaskLink( $task, $text ) {
 		global $mosConfig_live_site;
 
-		return '<a href="' .  $mosConfig_live_site . '/administrator/index2.php?option=com_acctexp&amp;task=' . $task . '" target="_blank" title="' . $text . '">' . $text . '</a>';
+		return '<a href="' .  $mosConfig_live_site . '/administrator/index2.php?option=com_acctexp&amp;task=' . $task . '" title="' . $text . '">' . $text . '</a>';
 	}
 
 	/**
