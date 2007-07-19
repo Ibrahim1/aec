4f603cf59cf1f541db5d894c4f8c3b2e0695f778
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2007-07-19 23:11:54 +0000

    fix install bug that would try to create a settings column again and again
    git-svn-id: http://joomlacode.org/svn/aec/trunk@285 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/install.acctexp.php
+++ src/install.acctexp.php
@@ -916,30 +916,31 @@ function com_install() {
 		$database->loadObject($result);
 
 		if( !( strcmp( $result->Field, 'settings' ) === 0 ) ) {
-			$cfg = new Config_General($database);
-
 			$columns = array("transferinfo", "initialexp", "alertlevel1", "alertlevel2", "alertlevel3", "gwlist", "customintro", "customthanks", "customcancel", "bypassintegration", "simpleurls", "expiration_cushion", "currency_code", "heartbeat_cycle", "tos", "require_subscription", "entry_plan", "plans_first", "transfer", "checkusername", "activate_paid" );
 
+			$newcfg = array();
 			foreach ($columns as $column) {
 				$result = null;
 				$database->setQuery("SHOW COLUMNS FROM #__acctexp_config LIKE '" . $column . "'");
 				$database->loadObject($result);
 				if(strcmp($result->Field, $column) === 0) {
 					$database->setQuery( "SELECT " . $column . " FROM #__acctexp_config WHERE id='1'" );
-					$cfg->cfg[$column] = $database->loadResult();
+					$newcfg[$column] = $database->loadResult();
 
 					$database->setQuery("ALTER TABLE #__acctexp_config DROP COLUMN " . $column);
 					if( !$database->query() ) {
 				    	$errors[] = array( $database->getErrorMsg(), $query );
 					}
 				}
-
-				$database->setQuery("ALTER TABLE #__acctexp_config ADD `settings` text");
-				if( !$database->query() ) {
-			    	$errors[] = array( $database->getErrorMsg(), $query );
-				}
 			}
 
+			$database->setQuery("ALTER TABLE #__acctexp_config ADD `settings` text");
+			if( !$database->query() ) {
+		    	$errors[] = array( $database->getErrorMsg(), $query );
+			}
+
+			$cfg = new Config_General($database);
+			$cfg->cfg = $newcfg;
 			$cfg->saveSettings();
 		}
 
