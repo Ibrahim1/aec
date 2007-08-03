383baee510b8608015584590c28b41aba5f019a0
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2007-08-03 13:29:07 +0000

    adding rokreporter thingy
    git-svn-id: http://joomlacode.org/svn/aec/trunk@346 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ rokreporter/report.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * @version		$Id: report.php 1 2007-05-28 13:12:48Z Shiny Black Shoe $
+ * @package		RokReporter
+ * @copyright	(C) 2007, Shiny Black Shoe. Modified from RokReporter inbuilt reports
+ * @license		GNU General Public License
+ */
+
+// ensure this file is being included by a parent file
+defined( '_VALID_MOS' ) or die( 'Direct access not allowed' );
+
+/**
+ * Reporting class
+ * @package		RokReporter
+ */
+class AEC_subscribers_Report extends Report
+{
+	/** @var string The report id (folder name); */
+	var $id = 'AEC_subscribers';
+
+	function ExportToCSV( $data )
+	{
+		$CRLF = "\n";
+		$nRows = count( $data );
+		if ($nRows > 0) {
+			$nCols = count( $data[0] );
+			$lines = array();
+			$labels = array();
+			$header = array();
+
+				foreach ($data[0] as $key => $value)
+				{
+					$labels[$key]='"'.$key.'"';
+				}
+				$header[] = implode(',',$labels);
+
+			for ($row = 0; $row < $nRows; $row++) {
+				foreach ($data[$row] as $key => $value) {
+					$cell = $data[$row][$key];
+				//	echo $key.":".$value;
+					$cell = str_replace( "\r\n", "\r", $cell );
+					$cell = str_replace( "\r", "\n", $cell );
+					$cell = str_replace( "\n", ";", $cell );
+					$cell = str_replace( '"', "'", $cell );
+					$cell = '"'.$cell.'"';
+				//	echo $cell;
+					$data[$row][$key]=$cell;
+				}
+				$lines[] = implode( ',', $data[$row] );
+				//echo "<br />";
+			}
+			$lines=array_merge($header,$lines);
+			return implode( $CRLF, $lines );
+
+		}
+		return '';
+	}
+
+	/**
+	 * Runs the report
+	 * @param array An array of system variables
+	 */
+	function run( &$vars )
+	{
+		// set the default ordering
+		if (empty( $vars['orderCol'] )) {
+			$vars['orderCol'] = 'search_term';
+		}
+
+		global $mosConfig_absolute_path;
+		$database = &$this->getDBO();
+		$query = "";
+		$query .= "SELECT DISTINCT(#__users.id), #__users.*, ";
+
+		// Check if CB installed...
+		if (file_exists( $mosConfig_absolute_path . "/components/com_comprofiler/comprofiler.php"))  {
+			$query .= $query."#__comprofiler.*, ";
+		}
+
+		$query .= "#__acctexp_subscr.*, #__acctexp.* ";
+		$query .= "FROM #__users ";
+		$query .= "LEFT JOIN #__acctexp ON #__users.id = #__acctexp.userid ";
+		$query .= "LEFT JOIN #__acctexp_subscr ON #__users.id = #__acctexp_subscr.userid ";
+
+		// Check if CB installed...
+		if (file_exists( $mosConfig_absolute_path . "/components/com_comprofiler/comprofiler.php")) {
+			$query .= "LEFT JOIN #__comprofiler ON #__users.id = #__comprofiler.user_id ";
+		}
+
+		$query .= "GROUP BY #__users.id";
+
+		$database->setQuery( $query );
+		$rows = $database->loadAssocList();  //this is exporting data with column heading
+		$buffer = trim( ExportToCSV( $rows ) );
+
+		$filename = "AEC_subscribers";
+		        header( "Content-type: application/octet-stream" );
+		        header( "Content-Length: ".strlen( $buffer ) );
+		        header( "Content-disposition: attachment; filename=$filename-".date("Y-m-d").".csv" );
+		        header( "Pragma: no-cache" );
+		        header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
+		        header( "Expires: 0" );
+		       echo $buffer;
+		        exit( 0 );
+		}
+}
+
+?>
