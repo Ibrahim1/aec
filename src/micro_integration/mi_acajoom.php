14650fb2bfd0ff2bed54d213a87840eea46aae3c
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-01-16 11:49:58 +0000

    fix for Acajoom MI - adding a user to a list was not working
    git-svn-id: http://joomlacode.org/svn/aec/trunk@721 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/micro_integration/mi_acajoom.php
+++ src/micro_integration/mi_acajoom.php
@@ -122,6 +122,8 @@ class mi_acajoom
 		. ' (type, subscriber_id, list_id, mailing_id, issue_nb, send_date, suspend, delay, acc_level, published, params)'
 		. ' VALUES(\'1\', \'' . $subscriber_id . '\', \'' . $list_id . '\', \'0\', \'0\', \'0000-00-00 00:00:00\', \'0\', \'0\', \'0\', \'0\', \'\' )'
 		;
+		$database->setQuery( $query );
+		return $database->loadResult();
 	}
 
 	function deleteFromList( $subscriber_id, $list_id )
