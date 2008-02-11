b96f7c132059412c3f88223c4340e402a3f7eea2
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-02-11 21:29:37 +0000

    starting work on paysite cash
    git-svn-id: http://joomlacode.org/svn/aec/trunk@823 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ src/processors/paysite_cash.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * @version $Id: paysite_cash.php
+ * @package AEC - Account Expiration Control / Subscription management for Joomla
+ * @subpackage Payment Processors
+ * @author David Deutsch <skore@skore.de>
+ * @copyright 2004-2007 David Deutsch
+ * @license http://www.gnu.org/copyleft/gpl.html. GNU Public License
+ */
+
+// Copyright (C) 2006-2007 David Deutsch
+// All rights reserved.
+// This source file is part of the Account Expiration Control Component, a  Joomla
+// custom Component By Helder Garcia and David Deutsch - http://www.globalnerd.org
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// Please note that the GPL states that any headers in files and
+// Copyright notices as well as credits in headers, source files
+// and output (screens, prints, etc.) can not be removed.
+// You can extend them with your own credits, though...
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+// The "GNU General Public License" (GPL) is available at
+// http://www.gnu.org/copyleft/gpl.html.
+
+// Dont allow direct linking
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' );
+
+class processor_paysite_cash extends URLprocessor
+{
+	function info()
+	{
+		$i = array();
+		$i['longname'] = _CFG_PAYSITE_CASH_LONGNAME;
+		$i['statement'] = _CFG_PAYSITE_CASH_STATEMENT;
+		$i['description'] = _CFG_PAYSITE_CASH_DESCRIPTION;
+		$i['currencies'] = 'USD';
+		$i['languages'] = 'AU,DE,FR,IT,GB,ES,US';
+		$i['cc_list'] = 'visa,mastercard,discover,americanexpress,echeck';
+		$i['notify_trail_thanks'] = 1;
+
+		return $i;
+	}
+
+	function settings()
+	{
+		$s = array();
+		$s['merchantid']	= "merchantid";
+		$s['resellerid']	= "resellerid";
+		$s['siteid']		= "siteid";
+		$s['secretcode']	= "secretcode";
+		$s['use_ticketsclub']	= 1;
+
+		return $s;
+	}
+
+	function backend_settings( $cfg )
+	{
+		$s = array();
+		$s['merchantid']		= array( 'inputC' );
+		$s['merchant']			= array( 'inputC' );
+		$s['siteid']			= array( 'inputC' );
+		$s['secretcode']		= array( 'inputC' );
+		$s['use_ticketsclub']	= array( 'list_yesno' );
+
+		$rewriteswitches	= array( 'cms', 'user', 'expiration', 'subscription', 'plan' );
+        $s['rewriteInfo']	= array( 'fieldset', _AEC_MI_REWRITING_INFO, AECToolbox::rewriteEngineInfo( $rewriteswitches ) );
+		return $s;
+	}
+
+	function CustomPlanParams()
+	{
+		$p = array();
+		$p['paysite_cash_product']	= array( 'inputC' );
+
+		return $p;
+	}
+
+	function createGatewayLink( $int_var, $cfg, $metaUser, $new_subscription )
+	{
+		// Payment Plans are required to have a productid assigned
+		if ( empty( $int_var['planparams']['paysite_cash_product'] ) ) {
+			$product = $cfg['siteid'];
+		} else {
+			$product = $int_var['planparams']['paysite_cash_product'];
+		}
+
+		$var['post_url'] = "https://secure.paysite_cash.com/cgi-bin/vtjp.pl?";
+		$var['paysite_cash_id'] = $cfg['merchantid'];
+		$var['montant'] = $product;
+		$var['paysite_cash_website'] = $cfg['siteid'];
+		$var['paysite_cash_usercode'] = $metaUser->cmsUser->username;
+		$var['paysite_cash_passcode'] = "xxxxxxxx";
+		$var['paysite_cash_custom1'] = $int_var['invoice'];
+
+		return $var;
+	}
+
+	function parseNotification( $post, $cfg )
+	{
+		$res = explode(":", $_GET['vercode']);
+
+		$secret		= $res[2];
+		$action     = $res[3];
+		$amount     = $res[4];
+		$payment_id = $res[5];
+		$pnref 	    = $res[6];
+
+		$response = array();
+		$response['invoice'] = $payment_id;
+
+		return $response;
+	}
+
+	function validateNotification( $response, $post, $cfg, $invoice )
+	{
+		$res = explode(":", $_GET['vercode']);
+
+		if( $cfg['secretcode'] == $res[2] ) {
+			$response['valid'] = 1;
+		} else {
+			$response['valid'] = 0;
+			$response['pending_reason'] = 'INVALID SECRET WORD, provided: ' . $res[3];
+		}
+
+		switch ( $res[3] ) {
+			case 'add':
+				$response['amount_paid'] = $res[4];
+				break;
+			case 'cancel':
+				$response['cancel'] = 1;
+				$response['valid'] = 0;
+				break;
+			case 'delete':
+				$response['delete'] = 1;
+				$response['valid'] = 0;
+				break;
+			case 'rebill':
+				$response['amount_paid'] = $res[4];
+				break;
+		}
+
+		return $response;
+	}
+
+}
+
+?>
