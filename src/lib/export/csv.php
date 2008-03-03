a1f28042a28a106a861cce0867628675506f1b0f
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-03-03 01:29:32 +0000

    Looks like we will have some exporting soon
    git-svn-id: http://joomlacode.org/svn/aec/trunk@903 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ src/lib/export/csv.php
@@ -0,0 +1,30 @@
+<?php
+class AECexport_cvs extends AECexport
+{
+	function AECexport_cvs()
+	{
+
+	}
+
+	function settings()
+	{
+
+	}
+
+	function settings_defaults()
+	{
+
+	}
+
+	function prepare_export()
+	{
+
+	}
+
+	function prepare_export()
+	{
+
+	}
+
+}
+?>
