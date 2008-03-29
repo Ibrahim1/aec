0d61c997b37f96c32fa22a206c0c83a1aab74747
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-03-29 23:57:42 +0000

    ok, really need to find a way to get processor notifications for XML on the way
    git-svn-id: http://joomlacode.org/svn/aec/trunk@1003 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/processors/authorize_arb.php
+++ src/processors/authorize_arb.php
@@ -309,6 +309,21 @@ class processor_authorize_arb extends XMLprocessor
 			Payment_HTML::error( 'com_acctexp', $metaUser->cmsUser, $invoice, "An error occured while cancelling your subscription. Please contact the system administrator!", true );
 		}
 	}
+/*
+	function validateNotification( $response, $post, $cfg, $invoice )
+	{
+		if ( $post['x_subscription_paynum'] > 1 ) {
+			$x_response_code		= $post['x_response_code'];
+			$x_response_reason_text	= $post['x_response_reason_text'];
 
+			$response['valid'] = ($x_response_code == '1');
+		} else {
+			$response['valid'] = 0;
+			$response['duplicate'] = true;
+		}
+
+		return $response;
+	}
+*/
 }
 ?>
