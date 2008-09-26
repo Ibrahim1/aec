e2a3f91106a334d9ccfcd01bf023d7c8a41fec77
mtk <mtk@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-09-26 17:54:26 +0000

    [#0000054] - added a new plugin with a new ErrorHandle for this case
    git-svn-id: http://joomlacode.org/svn/aec/trunk@1451 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ src_errorHandler/aecErrorHandler.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * @version $Id: aecErrorHandler.php
+ * @package AEC - Account Control Expiration - Joomla 1.5 Plugins
+ * @subpackage User
+ * @copyright 2006-2008 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.globalnerd.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+ */
+
+
+// Check to ensure this file is included in Joomla!
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Import library dependencies
+jimport('joomla.event.plugin');
+
+
+/**
+ * AEC Error Handler
+ *
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.globalnerd.org
+ * @package AEC Component
+ */
+class plgSystemAECErrorHandler extends JPlugin
+{
+	/**
+	 * Constructor
+	 *
+	 * For php4 compatability we must not use the __constructor as a constructor for plugins
+	 * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
+	 * This causes problems with cross-referencing necessary for the observer design pattern.
+	 *
+	 * @param object $subject The object to observe
+	 * @param array  $config  An array that holds the plugin configuration
+	 * @since 1.5
+	 */
+	function plgSystemAECErrorHandler(& $subject, $config) 
+	{
+		parent::__construct($subject, $config);
+	}
+	
+	function onAfterInitialise()
+	{
+		global $mainframe;
+
+		// Get the message queue
+		$messages = $mainframe->getMessageQueue();
+		
+		// Build the sorted message list
+		if (is_array($messages) && count($messages)) {
+			foreach ($messages as $msg)
+			{
+				if (isset($msg['type']) && isset($msg['message'])) {
+					if($msg['message'] == JText::_("YOU MUST LOGIN FIRST")) {
+						$mainframe->redirect("index.php?option=com_acctexp&task=NotAllowed" );
+					}					
+				}
+			}
+		}
+
+//		JError::setErrorHandling(E_ALL, 'callback', array($this, 'aecErrorHandler'));
+	}
+	
+	function aecErrorHandler(& $error){
+		if (file_exists( JPATH_ROOT.DS."components".DS."com_acctexp".DS."acctexp.class.php" )) {
+			global $mainframe;
+			
+			if($error->message == JText::_("ALERTNOTAUTH")) {
+				$mainframe->redirect("index.php?option=com_acctexp&task=NotAllowed" );
+			}
+
+		}
+	}
+
+}
+
+?>
