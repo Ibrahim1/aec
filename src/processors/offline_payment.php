f9c617ab08c68b69b877e79c74370b89fce9742d
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2008-11-11 16:17:47 +0000

    #0000181: Waiting Plan in Offline Payment - doesn't apply
    git-svn-id: http://joomlacode.org/svn/aec/trunk@1587 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/processors/offline_payment.php
+++ src/processors/offline_payment.php
@@ -53,15 +53,17 @@ class processor_offline_payment extends processor
 
 			$metaUser = new metaUser( $objInvoice->userid );
 
-			$metaUser->objSubscription->applyUsage( $this->settings['waitingplan'], 'none', 0 );
+			if ( !$metaUser->hasSubscription || in_array( $metaUser->objSubscription->status, array( 'Expired', 'Closed' ) ) ) {
+				$metaUser->objSubscription->applyUsage( $this->settings['waitingplan'], 'none', 0 );
 
-			$short	= 'waiting plan';
-			$event	= 'Offline Payment waiting plan assigned for ' . $objInvoice->invoice_number;
-			$tags	= 'processor,waitingplan';
-			$params = array( 'invoice_number' => $objInvoice->invoice_number );
+				$short	= 'waiting plan';
+				$event	= 'Offline Payment waiting plan assigned for ' . $objInvoice->invoice_number;
+				$tags	= 'processor,waitingplan';
+				$params = array( 'invoice_number' => $objInvoice->invoice_number );
 
-			$eventlog = new eventLog( $database );
-			$eventlog->issue( $short, $tags, $event, 2, $params );
+				$eventlog = new eventLog( $database );
+				$eventlog->issue( $short, $tags, $event, 2, $params );
+			}
 		}
 	}
 
