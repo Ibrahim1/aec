81d1cfea1467493d7e6fcb525470982d339f0d9b
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2009-01-06 00:08:02 +0000

    new verotel IP range
    git-svn-id: http://joomlacode.org/svn/aec/trunk@1765 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- src/processors/verotel.php
+++ src/processors/verotel.php
@@ -135,7 +135,7 @@ class processor_verotel extends URLprocessor
 
 	function validateNotification( $response, $post, $invoice )
 	{
-		if ( !AECToolbox::in_ip_range( '195.20.32.0', '﻿195.20.32.128' ) ) {
+		if ( !AECToolbox::in_ip_range( '195.20.32.0', '﻿195.20.32.256' ) ) {
 			$response['error'] = 1;
 			$response['errormsg'] = "Wrong IP tried to send notification: " . $_SERVER["REMOTE_ADDR"];
 			return $response;
