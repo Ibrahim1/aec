c5d1f8b17b98cd6319d1c4f7422c9bc1bf1bd7ba
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2009-03-16 21:14:38 +0000

    age restriction MI
    git-svn-id: http://joomlacode.org/svn/aec/trunk@1921 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ src/micro_integration/mi_age_restriction.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @version $Id: mi_age_restriction.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Micro Integrations - Age Restriction
+ * @copyright 2006-2008 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.globalnerd.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+ */
+
+// Dont allow direct linking
+( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
+
+class mi_age_restriction extends MI
+{
+	function Settings()
+	{
+		global $database;
+
+        $settings = array();
+		$settings['min_age']			= array( 'inputA' );
+		$settings['max_age']			= array( 'inputA' );
+		$settings['restrict_calendar']	= array( 'list_yesno' );
+
+		return $settings;
+	}
+
+	function getMIform()
+	{
+		global $database;
+
+		$settings = array();
+
+		$settings['birthday'] = array( 'inputC', _MI_MI_AGE_RESTRICTION_USERSELECT_BIRTHDAY_NAME, _MI_MI_AGE_RESTRICTION_USERSELECT_BIRTHDAY_DESC );
+
+		return $settings;
+	}
+
+	function verifyMIform( $params )
+	{
+		global $database;
+
+		$return = array();
+
+		$age = $this->getAge( $params['birthday'] );
+
+		if ( !empty( $this->settings['min_age'] ) ) {
+			if ( $age < $this->settings['min_age'] ) {
+				$return['error'] = "You must be at least " . $this->settings['min_age'] . " years old!";
+				return $return;
+			}
+		}
+
+		if ( !empty( $this->settings['max_age'] ) ) {
+			if ( $age > $this->settings['max_age'] ) {
+				$return['error'] = "You cannot purchase this if you are older than " . $this->settings['max_age'] . " years!";
+				return $return;
+			}
+		}
+
+		return $return;
+	}
+
+	function relayAction( $request, $area )
+	{
+		if ( $area == 'action' ) {
+			$age = $this->getAge( $request->params['birthday'] );
+
+			$due_date = strtotime( "+" . $this->settings['max_age'] . " years", strtotime( $request->params['birthday'] ) );
+
+			$this->issueEvent( $request, 'BirthdayExpiration', $due_date );
+		}
+
+		return true;
+	}
+
+	function aecEventHookBirthdayExpiration( $request )
+	{
+		if ( !empty( $request->metaUser->focusSubscription->id ) ) {
+			return $request->metaUser->focusSubscription->expire();
+		}
+
+		return null;
+	}
+
+	function getAge( $bd )
+	{
+		return (int) ( strtotime( $bd ) - time() ) / 31556926;
+	}
+}
+?>
