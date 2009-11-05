cb0c548cbdfb6f6cb411772fbc00fbb8f50dc2da
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2009-11-05 17:19:00 +0000

    #0000482: Trvial things with the new build
    git-svn-id: http://joomlacode.org/svn/aec/trunk@2423 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- newsrc/code/administrator/components/com_acctexp/lang/spanish.php
+++ newsrc/code/administrator/components/com_acctexp/lang/spanish.php
@@ -960,6 +960,7 @@ define( '_AEC_CUSTOM_INVOICE_BEFORE_CONTENT', 'Invoice/Receipt for:');
 define( '_AEC_CUSTOM_INVOICE_AFTER_CONTENT', 'Thank you very much for choosing our service!');
 define( '_AEC_CUSTOM_INVOICE_FOOTER', ' - Add your company information here - ');
 
+define( '_CFG_GENERAL_INVOICE_PAGE_TITLE', 'Invoice');
 define( '_CFG_GENERAL_INVOICE_HEADER_NAME', 'Invoice Header');
 define( '_CFG_GENERAL_INVOICE_HEADER_DESC', 'Header Text for the Invoice Printout');
 define( '_CFG_GENERAL_INVOICE_AFTER_HEADER_NAME', 'Invoice After Header');
