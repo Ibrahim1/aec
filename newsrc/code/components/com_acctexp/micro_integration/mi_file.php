c5468e0af533b7c4166259f4dc03c52164e21d74
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-01-01 16:18:20 +0000

    working on file MI
    git-svn-id: http://joomlacode.org/svn/aec/trunk@2579 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- newsrc/code/components/com_acctexp/micro_integration/mi_file.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_file.php
@@ -2,7 +2,7 @@
 /**
  * @version $Id: mi_file.php
  * @package AEC - Account Control Expiration - Membership Manager
- * @subpackage Micro Integrations - Eventlog
+ * @subpackage Micro Integrations - File Manipulation
  * @copyright 2006-2008 Copyright (C) David Deutsch
  * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
  * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
@@ -40,13 +40,13 @@ class mi_file extends MI
 
 	function relayAction( $request )
 	{
-		if ( !isset( $this->settings['short'.$request->area] ) ) {
+		if ( !isset( $this->settings['path'.$request->area] ) ) {
 			return null;
 		}
 
 		$database = &JFactory::getDBO();
 
-		$rewriting = array( 'short', 'tags', 'text', 'params' );
+		$rewriting = array( 'path', 'append', 'content' );
 
 		foreach ( $rewriting as $rw_name ) {
 			$this->settings[$rw_name.$request->area] = AECToolbox::rewriteEngineRQ( $this->settings[$rw_name.$request->area], $request );
@@ -54,6 +54,16 @@ class mi_file extends MI
 
 		$log_entry = new EventLog( $database );
 		$log_entry->issue( $this->settings['short'.$request->area], $this->settings['tags'.$request->area], $this->settings['text'.$request->area], $this->settings['level'.$request->area], $this->settings['params'.$request->area], $this->settings['force_notify'.$request->area], $this->settings['force_email'.$request->area] );
+
+		if ( $this->settings['append'.$request->area] ) {
+			$file = fopen( $this->settings['path'.$request->area], "a" );
+		} else {
+			$file = fopen( $this->settings['path'.$request->area], "w" );
+		}
+
+		fwrite( $file, $this->settings['content'.$request->area] );
+
+		return fclose( $file );
 	}
 }
 ?>
