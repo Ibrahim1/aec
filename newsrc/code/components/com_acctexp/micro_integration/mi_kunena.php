6c8d951c230001bdb32fbb8fb218e7b597a1afa5
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-02-09 21:23:57 +0000

    pretty much finished kunena MI
    git-svn-id: http://joomlacode.org/svn/aec/trunk@2615 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- newsrc/code/components/com_acctexp/micro_integration/mi_kunena.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_kunena.php
@@ -1,24 +1,23 @@
 <?php
-/*
- * <name>mi_kunena</name>
- * <creationDate>January 2010</creationDate>
- * <version>1.5.0</version>
- * <author>Joel Bassett</author>
- * <authorEmail>joel@aqsg.com.au</authorEmail>
- * <authorUrl>http://www.aqsg.com.au</authorUrl>
- * <copyright>(C) 2005-2010 Australian Quality Solutions Group. All rights reserved.</copyright>
- * <license>AGPLv3 - http://www.aqsg.com.au/products/simple-meta-management-suite/licence.html</license>
+/**
+ * @version $Id: mi_kunena.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Micro Integrations - Kunena
+ * @copyright Copyright (C) 2010 David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
  */
+
 // Dont allow direct linking
 defined('_JEXEC') OR defined( '_VALID_MOS' ) OR die( 'Direct Access to this location is not allowed.' );
 
-class mi_kunena
+class mi_kunena extends MI
 {
 	function Info()
 	{
 		$info = array();
-		$info['name'] = 'Kunena Forum';
-		$info['desc'] = 'Change the Users Rank in Kunena';
+		$info['name'] = _AEC_MI_NAME_KUNENA;
+		$info['desc'] = _AEC_MI_DESC_KUNENA;
 
 		return $info;
 	}
@@ -27,75 +26,99 @@ class mi_kunena
 	{
 		$settings = array();
 
-		if(!include_once(rtrim(JPATH_ROOT, DS) . DS . 'components' . DS . 'com_kunena' . DS . 'lib' . DS . 'kunena.user.class.php')){
+		if ( !include_once( rtrim(JPATH_ROOT, DS) . DS . 'components' . DS . 'com_kunena' . DS . 'lib' . DS . 'kunena.user.class.php' ) ) {
 			echo 'This module can not work without the Kunena Forum Component';
 			return $settings;
-		};
-
-		$database = &JFactory::getDBO();
-		$database->setQuery('SELECT * FROM ' . $database->nameQuote('#__fb_ranks'));
-		$ranks = $database->loadObjectList();
-
-		$listsValues[] = JHTML::_('select.option', 0, '--- Not Set ---' );
-
-		foreach ($ranks as $id=>$row) {
-			$listsValues[] = JHTML::_('select.option', $row->rank_id, $row->rank_id . ' : ' . $row->rank_title );
 		}
 
-		$settings['sublists'] = array( 'list','[Activation] Change the user\'s rank to the selected rank','Select the rank you want to apply to the user on plan activation' );
-		$settings['unsublists'] = array( 'list','[Activation] Remove the user\'s from the following rank','Select the rank you want to remove the user from on plan activation' );
-		$settings['sublistsexpir'] = array( 'list','[Expiration] Change the user\'s rank to the selected rank','Select the rank you want to apply to the user on plan expiration' );
-		$settings['unsublistsexpir'] = array( 'list','[Expiration] Remove the user\'s from the following rank','Select the rank you want to remove the user from on plan expiration' );
+		$database = &JFactory::getDBO();
+		$database->setQuery( 'SELECT * FROM #__fb_ranks' );
 
-		$settings['lists']['sublists']		= JHTML::_('select.genericlist',   $listsValues, 'sublists[]', 'class="inputbox"', 'value', 'text', empty($this->settings['sublists']) ? '' : $this->settings['sublists']);
-		$settings['lists']['unsublists']	= JHTML::_('select.genericlist',   $listsValues, 'unsublists[]', 'class="inputbox"', 'value', 'text', empty($this->settings['unsublists']) ? '' : $this->settings['unsublists']);
-		$settings['lists']['sublistsexpir']		= JHTML::_('select.genericlist',   $listsValues, 'sublistsexpir[]', 'class="inputbox"', 'value', 'text', empty($this->settings['sublistsexpir']) ? '' : $this->settings['sublistsexpir']);
-		$settings['lists']['unsublistsexpir']	= JHTML::_('select.genericlist',   $listsValues, 'unsublistsexpir[]', 'class="inputbox"', 'value', 'text', empty($this->settings['unsublistsexpir']) ? '' : $this->settings['unsublistsexpir']);
+		$ranks = $database->loadObjectList();
+
+		$ranklist = array();
+		$ranklist[] = mosHTML::makeOption ( 0, "--- --- ---" );
+
+		foreach ( $ranks as $id => $row ) {
+			$ranklist[] = mosHTML::makeOption ( $row->rank_id, $row->rank_id . ': ' . $row->rank_title );
+		}
+
+		$settings['rank']	= array( 'list' );
+		$settings['unrank']	= array( 'list' );
+
+		$setting = $this->autoduplicatesettings( $settings );
+
+		foreach ( $settings as $k => $v ) {
+			if ( isset( $this->settings[$k] ) ) {
+				$settings['lists'][$k]	= mosHTML::selectList( $ranklist, $k, 'size="4" multiple="multiple"', 'value', 'text', '' );
+			} else {
+				$settings['lists'][$k]	= mosHTML::selectList( $ranklist, $k, 'size="4" multiple="multiple"', 'value', 'text', $this->settings[$k] );
+			}
+		}
+
+		$settings['rebuild']	= array( 'list_yesno' );
+		$settings['remove']		= array( 'list_yesno' );
 
 		return $settings;
 	}
 
-	function expiration_action( $request )
+	function relayAction( $request )
 	{
-		$this->changeRank($request->metaUser->userid, $this->settings['sublistsexpir'], $this->settings['unsublistsexpir']);
+		if ( !empty( $this->settings['rank' . $request->area] ) || !empty( $this->settings['unrank' . $request->area] ) ) {
+			$this->changeRank( $request->metaUser->userid, $this->settings['rank' . $request->area], $this->settings['unrank' . $request->area] );
+		}
 	}
 
-	function action( $request )
-	{
-		$this->changeRank($request->metaUser->userid, $this->settings['sublists'], $this->settings['unsublists']);
-	}
-
-	function changeRank($userid, $subscribe_array, $remove_array)
+	function changeRank( $userid, $add, $remove )
 	{
 		$database = &JFactory::getDBO();
 
-		if(empty($subscribe_array) AND empty($remove_array)) return;
+		$query = 'SELECT `userid`, `group_id`'
+				. ' FROM #__fb_users'
+				. ' WHERE `userid` = \'' . $userid . '\''
+				;
+		$database->setQuery( $query );
 
-		$remove = $remove_array[0];
-		$subscribe = $subscribe_array[0];
+		$kuser = $database->loadObject();
 
-		$database->setQuery('SELECT COUNT(*) FROM ' . $database->nameQuote('#__fb_users') . ' WHERE userid=' . $database->Quote($userid));
-		$count = $database->loadResult();
-		if ($count < 1) {
-			$query = 'INSERT INTO ' . $database->nameQuote('#__fb_users') . ' (' . $database->nameQuote('userid') . ', ' . $database->nameQuote('view') . ', ' . $database->nameQuote('moderator') . ', ' . $database->nameQuote('ordering') . ', ' . $database->nameQuote('posts') . ', ' . $database->nameQuote('karma') . ', ' . $database->nameQuote('karma_time') . ', ' . $database->nameQuote('group_id') . ', ' . $database->nameQuote('uhits') . ', ' . $database->nameQuote('gender') . ', ' . $database->nameQuote('birthdate') . ', ' . $database->nameQuote('rank') . ', ' . $database->nameQuote('hideEmail') . ', ' . $database->nameQuote('showOnline') . ') VALUES ';
-			if($remove) {
-				$query .= "(" . $database->Quote($userid) . ", " . $database->Quote('flat') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('1') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0001-01-01') . ", " . $database->Quote('0') . ", " . $database->Quote('1') . ", " . $database->Quote('1') . ")";
-				$database->setQuery($query);
-				$database->query();
-			} elseif($subscribe) {
-				$query .= "(" . $database->Quote($userid) . ", " . $database->Quote('flat') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('1') . ", " . $database->Quote('0') . ", " . $database->Quote('0') . ", " . $database->Quote('0001-01-01') . ", " . $database->Quote($subscribe) . ", " . $database->Quote('1') . ", " . $database->Quote('1') . ")";
-				$database->setQuery($query);
-				$database->query();
-			}
-		} else {
-			if($remove) {
-				$database->setQuery('UPDATE ' . $database->nameQuote('#__fb_users') . ' SET rank=' . $database->Quote('0') . ' WHERE userid=' . $database->Quote($userid));
-				$database->query();
-			} elseif($subscribe) {
-				$database->setQuery('UPDATE ' . $database->nameQuote('#__fb_users') . ' SET rank=' . $subscribe . ' WHERE userid=' . $userid);
-				$database->query();
+		$rank = $kuser->group_id;
+
+		if ( !empty( $remove ) && $kuser->userid ) {
+			if ( $remove == $rank ) {
+				$newrank = 0;
 			}
 		}
+
+		if ( !empty( $add ) && $kuser->userid ) {
+			if ( $add == $rank ) {
+				// Already in the correct usergroup
+				return null;
+			} else {
+				$newrank = $add;
+			}
+		}
+
+		if ( !isset( $rank ) && !empty( $add ) ) {
+			$rank = $add;
+		} else {
+			// Nothing to do
+			return null;
+		}
+
+		if ( $kuser->userid ) {
+			$query = 'UPDATE #__fb_users'
+					. ' SET `group_id` = \'' . $newrank . '\''
+					. ' WHERE `userid` = \'' . $userid . '\''
+					;
+		} else {
+			$query = 'INSERT INTO #__fb_users'
+					. ' ( `group_id` , `userid` )'
+					. ' VALUES (\'' . $newrank . '\', \'' . $userid . '\')'
+					;
+		}
+
+		$database->setQuery( $query );
+		$database->query();
 	}
 
 }
