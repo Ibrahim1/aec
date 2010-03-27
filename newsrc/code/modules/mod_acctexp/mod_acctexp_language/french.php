06d48ee79257b394f5d326b303991296beaff275
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-03-27 16:37:22 +0000

    restoring missing language files. this was the last time that I trust SVN to handle moves and renames.
    git-svn-id: http://joomlacode.org/svn/aec/trunk@2736 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ newsrc/code/modules/mod_acctexp/mod_acctexp_language/french.php
@@ -0,0 +1,5 @@
+<?php
+DEFINE ('_ACCOUNT_EXPIRES','Votre adhésion expire dans ');
+DEFINE ('_ACCOUNT_RENEWAL','Account Renewal');
+DEFINE ('_ACCOUNT_UNLIMIT','Accès illimité');
+?>
---
+++ newsrc/code/modules/mod_acctexp/mod_acctexp_language/simplified_chinese.php
@@ -0,0 +1,5 @@
+﻿<?php
+DEFINE ('_ACCOUNT_EXPIRES','帐号有效至 ');
+DEFINE ('_ACCOUNT_RENEWAL','Account Renewal');
+DEFINE ('_ACCOUNT_UNLIMIT','无限帐号');
+?>
