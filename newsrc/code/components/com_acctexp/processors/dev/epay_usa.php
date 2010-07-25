ee5e170a903b3b5785864262c84cc4eef5269ca3
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-07-25 18:59:21 +0000

    dev version of eWay USA
    git-svn-id: http://joomlacode.org/svn/aec/trunk@3025 df06b4cb-4433-0410-b9f2-cc2e200029ab

---
+++ newsrc/code/components/com_acctexp/processors/dev/epay_usa.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * @version $Id: usaepay.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Processors - Psigate
+ * @copyright 2007-2008 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+ */
+
+// Dont allow direct linking
+( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
+
+class processor_epay_usa extends POSTprocessor
+{
+	function info()
+	{
+		$info = array();
+		$info['name'] = "usaepay";
+		$info['longname'] = "usaepay";
+		$info['statement'] = "Make payments with USAEpay!";
+		$info['description'] = "USAEpay";
+		$info['cc_list'] = "visa,mastercard,discover,jcb";
+		$info['currencies'] = "USD";
+		$info['recurring'] = 1;
+		$info['notify_trail_thanks'] = 1;
+
+		return $info;
+	}
+
+	function settings()
+	{
+		$settings = array();
+		$settings['testmode']		= 0;
+		//$settings['StoreKey']		= "StoreKey";
+		//$settings['secretWord']	= "Secret Word";
+		$settings['customparams']	= "";
+
+		return $settings;
+	}
+
+	function backend_settings()
+	{
+		$settings = array();
+
+		$settings['testmode']		= array( "list_yesno", "Test Mode", "Operate in USAePay TEST mode" );
+		$settings['StoreKey']		= array( "inputC","Store Key","Your Alphanumeric ID assigned by USAePay" );
+		$settings['secretWord']		= array( "inputC","Secret Word","Used to encrypt and protect transactions" );
+		$settings['customparams']	= array( 'inputD' );
+		return $settings;
+	}
+
+      function recurringDateCalculation() {
+         $recurringChargeDate = date("Ymd", time() + (15 * 24 * 60 * 60));
+         print_r("<br/><br/>15 days from today = ".$recurringChargeDate);
+         return $recurringChargeDate;
+      }
+
+	function createGatewayLink( $request )
+	{
+		if ( $this->settings['testmode'] ) {
+			$var['post_url']	= "https://sandbox.usaepay.com/interface/epayform/52a8f3d1Ut6yss5juANQ/";
+                        $var['UMkey']           = "52a8f3d1Ut6yQ1KxuANQ";                        
+                           
+		} else {
+			$var['post_url']	= "https://secure.usaepay.com/interface/epayform/bQC545L1cu1tkjmy64/";
+			$var['UMkey']           = "bQC545L1cu10p2A3Itkjmy64";
+			
+		}
+
+
+                $var['UMinvoice']              	= $request->invoice->invoice_number;
+                $var['UMdescription']           = "Subscription at Help-My-Finances.com";
+		$var['UMamount']		= 1.95;
+		$var['UMbillamount']            = 9.95;
+                
+            $var['UMrecurring']             = "yes";                                          
+            $var['UMstart']                 = date("Ymd", time() + (15 * 24 * 60 * 60));  //UMstart Must be entered in YYYYMMDD
+               		
+		return $var;
+	}
+
+
+	function parseNotification ( $post )
+	{
+		$ReturnCode	= aecGetParam('ReturnCode', 'NA');
+		$ErrMsg		= aecGetParam('ErrMsg', 'NA');
+		$FullTotal	= 1.95;   //aecGetParam('FullTotal', 'NA');  //   <========== working
+		$CardNumber	= aecGetParam('CardNumber', 'NA');
+		$OrderID	= aecGetParam('UMinvoice', 'NA');
+
+		$checksum	= md5($OrderID . $FullTotal);
+
+		$response = array();
+		$response['TransRefNumber']	= aecGetParam('UMrefNum', 'NA');
+		$response['Approved']		= aecGetParam('UMstatus', 'NA');
+		$response['FullTotal']		= $FullTotal;
+		$response['CardNumber']		= $CardNumber;
+		$response['OrderID']		= $OrderID;
+		$response['invoice']		= aecGetParam('UMinvoice', 'NA');
+                //$response['valid']              = aecGetParam('valid', '0');
+		//$validate			= md5($this->settings['secretWord'] . $FullTotal);
+		//$response['valid']	= (strcmp($validate, $checksum) == 0);
+                
+		//if ( $response['valid'] = 1 )
+                if  ($response['Approved'] == "Approved" ) {
+			//if ( substr( $ReturnCode, 0, 1 ) == "Y" ) {
+			        print_r("Response was...". $response['Approved'] . "<br/>");
+				print_r("<b>Thankyou! - Your Card was approved</b><br/>");
+				print_r("</br>");
+				//print_r("<b>Card No:</b>". $CardNumber . "<br/>");
+				print_r("<b>Total Charged:</b>". $FullTotal . "<br/>");
+				//print_r("<br/>");
+         			$response['valid'] = 1;
+	          		$response['pending']= 0;
+                                  				
+			//} else {
+			//	$response['valid']		= 0;
+			//	$response['pending']		= 1;
+			//	$response['pending_reason']	= $ErrMsg;
+			//	print_r("<b>Transaction Declined <br/>Reason: </b>" .$ErrMsg . "<br/>");
+			//}
+		} else  {
+			$response['valid'] = 0;
+			$response['pending']= 1;
+			$response['pending_reason']=$ErrMsg;
+			print_r("<b>Transaction Declined (cs)<br/>Reason: </b>" .$ErrMsg . "<br/>");
+
+		}
+
+		//print_r("<b>TransRefNumber:</b>". $response['TransRefNumber'] . "<br/>");
+		print_r("<b>Invoice:</b>". $response['invoice'] . "<br/><br/><br/>");
+		//print_r("<b>OrderID:</b>". $response['OrderID']. "<br/>");
+		//print_r("<b>Approval:</b>". $response['Approved'] . "<br/>");		
+		//print_r("<b>Valid Code:</b>". $response['valid'] . "<br/>");
+        	//print_r("<b>Pending Code:</b>". $response['pending']. "<br/>");
+
+		return $response;
+	}
+
+}
+?>
