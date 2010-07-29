04ccca69b57f23e3b134f249a681a2ea0c4d58f4
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-07-29 06:28:37 +0000

    client working on securepay
    git-svn-id: http://joomlacode.org/svn/aec/trunk@3033 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- newsrc/code/components/com_acctexp/processors/dev/securepay.php
+++ newsrc/code/components/com_acctexp/processors/dev/securepay.php
@@ -1,136 +1,293 @@
-<?php
+ 	<?php
+/**
+ * @version $Id: securepayxml.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Processors - eWay XML
+ * @copyright 2006-2008 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+ */
+
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
 
+/**
+* AcctExp Component
+* @package AEC - Account Control Expiration - Membership Manager
+* @subpackage processor
+* @copyright 2006-2008 Copyright (C) David Deutsch
+* @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+* @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+**/
+
 class processor_securepay extends XMLprocessor
 {
-	 function info()
-	 {
-			$info = array();
-			$info['name']			= 'securepayxml';
-			$info['longname']		= _CFG_SECUREPAY_LONGNAME;
-			$info['statement']		= _CFG_SECUREPAY_STATEMENT;
-			$info['description']	= _CFG_SECUREPAY_DESCRIPTION;
-			$info['cc_list']		= 'visa,mastercard';
-			$info['recurring']		= 0;
+	function info()
+	{
+		$info = array();
+		$info['name']			= 'securepayxml';
+		$info['longname']		= _CFG_SECUREPAY_LONGNAME;
+		$info['statement']		= _CFG_SECUREPAY_STATEMENT;
+		$info['description']	= _CFG_SECUREPAY_DESCRIPTION;
+		$info['cc_list']		= 'visa,mastercard';
+		$info['recurring']		= 0;
 
-			return $info;
-	 }
+		return $info;
+	}
 
-	 function settings()
-	 {
-			$settings = array();
-			$settings['testmode']		= "1";
-			$settings['merchantid']		= '';
-			$settings['password']		= '';
-			$settings['tax']			= "10";
-			$settings['testAmount']		= "00";
-			$settings['item_name']		= sprintf( _CFG_PROCESSOR_ITEM_NAME_DEFAULT, '[[cms_live_site]]', '[[user_name]]', '[[user_username]]' );
-			$settings['rewriteInfo']	= '';
-			$settings['SiteTitle']		= '';
+	function settings()
+	{
+		$settings = array();
+		$settings['testmode']		= "1";
+		$settings['merchantid']		= '';
+		$settings['password']		= '';
+		$settings['tax']			= "10";
+		$settings['testAmount']		= "00";
+		$settings['item_name']		= sprintf( _CFG_PROCESSOR_ITEM_NAME_DEFAULT, '[[cms_live_site]]', '[[user_name]]', '[[user_username]]' );
+		$settings['rewriteInfo']	= '';
+		$settings['SiteTitle']		= '';
 
-			return $settings;
-	 }
+		return $settings;
+	}
 
-	 function backend_settings()
-	 {
-			$settings = array();
-			$settings['testmode']	= array( 'list_yesno' );
-			$settings['merchantid']	= array( 'inputC' );
-			$settings['password']	= array( 'inputC' );
-			$settings['SiteTitle']	= array( 'inputC' );
-			$settings['item_name']	= array( 'inputE' );
+	function backend_settings()
+	{
+		$settings = array();
+		$settings['testmode']		= array( 'list_yesno' );
+		$settings['merchantid']		= array( 'inputC' );
+		$settings['password']		= array( 'inputC' );
+		$settings['SiteTitle']		= array( 'inputC' );
+		$settings['item_name']		= array( 'inputE' );
 
-			$rewriteswitches         = array( 'cms', 'user', 'expiration', 'subscription', 'plan');
-			$settings = AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
+ 		$rewriteswitches			= array( 'cms', 'user', 'expiration', 'subscription', 'plan');
+		$settings = AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
 
-			return $settings;
-	 }
+		return $settings;
+	}
 
-	 function createRequestXML( $request )
-	 {
-			$order_total = $request->int_var['amount'] * 100;
+	function createRequestXML( $request )
+	{
 
-			$xml .= '<?xml version="1.0" encoding="UTF-8"?>';
-			$xml .= '<SecurePayMessage>';
+		$order_total = $request->int_var['amount'] * 100;
 
-			$xml .= '<MessageInfo>';
-			$xml .= '<messageID>8af793f9af34bea0cf40f5fb5c630c</messageID>';
-			$xml .= '<messageTimestamp>'.time().'</messageTimestamp>';
-			$xml .= '<timeoutValue>60</timeoutValue>';
-			$xml .= '<apiVersion>xml-4.2</apiVersion>';
-			$xml .= '</MessageInfo>';
+		$xml .= '<?xml version="1.0" encoding="UTF-8"?>';
+		$xml .= '<SecurePayMessage>';
 
-			$xml .= '<MerchantInfo>';
-			// $xml .= '<merchantID>'.$this->$settings['testmerchantid'].'</merchantID>';
-			// $xml .= '<password>'.$this->$settings['password'].'</password>';
-			$xml .= '<merchantID>[xxxxxxxxxx]</merchantID>';
-			$xml .= '<password>[xxxxxxxxxx]</password>';
-			$xml .= '</MerchantInfo>';
+		$xml .= '<MessageInfo>';
+		$xml .= '<messageID>'.$request->invoice->invoice_number.'</messageID>';
+		$xml .= '<messageTimestamp>'.date("YmdGis")."000+1000".'</messageTimestamp>';
+		$xml .= '<timeoutValue>60</timeoutValue>';
+		$xml .= '<apiVersion>xml-4.2</apiVersion>';
+		$xml .= '</MessageInfo>';
 
-			$xml .= '<RequestType>Payment</RequestType>';
+		$xml .= '<MerchantInfo>';
+		$xml .= '<merchantID>xxxxxxxxxx</merchantID>';
+		$xml .= '<password>xxxxxxxxxx</password>';
+		$xml .= '</MerchantInfo>';
 
-			$xml .= '<Payment>';
-			$xml .= '<TxnList count="1">';
-			$xml .= '<txnType>0</txnType>';
-			$xml .= '<txnSource>0</txnSource>';
-			$xml .= '<amount>'.$order_total.'</amount>';
-			$xml .= '<purchaseOrderNo>'.$request->invoice->invoice_number.'</purchaseOrderNo>';
+		$xml .= '<RequestType>Payment</RequestType>';
 
-			$xml .= '<CreditCardInfo>';
-			//$xml .= '<cardHolder>'.$request->int_var['params']['billFirstName'] . ' ' . $request->int_var['params']['billLastName'].'</cardNumber>';
-			$xml .= '<cardNumber>'.$request->int_var['params']['cardNumber'].'</cardNumber>';
-			$xml .= '<expiryDate>'.$request->int_var['params']['expirationYear'].'/'.$request->int_var['params']['expirationMonth'].'</expiryDate>';
-			$xml .= '</CreditCardInfo>';
+		$xml .= '<Payment>';
+		$xml .= '<TxnList count="1">';
+		$xml .= '<Txn ID="1">';
+		$xml .= '<txnType>0</txnType>';
+		$xml .= '<txnSource>0</txnSource>';
+		$xml .= '<amount>'.$order_total.'</amount>';
+		$xml .= '<purchaseOrderNo>'.$request->invoice->invoice_number.'</purchaseOrderNo>';
 
-			$xml .= '</Txn>';
-			$xml .= '</TxnList>';
-			$xml .= '</Payment>';
-			$xml .= '</SecurePayMessage>';
+		$xml .= '<CreditCardInfo>';
+		$xml .= '<cardNumber>'.$request->int_var['params']['cardNumber'].'</cardNumber>';
+		$xml .= '<expiryDate>'.$request->int_var['params']['expirationYear'].'/'.$request->int_var['params']['expirationMonth'].'</expiryDate>';
+		$xml .= '</CreditCardInfo>';
 
-			$xml .= '</SecurePayMessage>';
+		$xml .= '</Txn>';
+		$xml .= '</TxnList>';
+		$xml .= '</Payment>';
 
-			return $xml;
-	 }
+		$xml .= '</SecurePayMessage>';
 
-	 function transmitRequestXML( $xml, $request )
-	 {
-			if ( $this->settings['testmode'] ) {
-				 $url = 'http://test.securepay.com.au/xmlapi/payment';
+		return $xml;
+	}
+
+	function transmitRequestXML( $xml, $request )
+	{
+		if ( $this->settings['testmode'] ) {
+			$url = 'https://test.securepay.com.au/xmlapi/payment';
+		} else {
+			$url = 'https://www.securepay.com.au/xmlapi/payment';
+		}
+
+		$response = array();
+
+		if ( $objResponse = simplexml_load_string( $this->transmitRequest( $url, '', $xml ) ) ) {
+			$response['amount_paid'] = $objResponse->amount / 100;
+			$response['invoice'] = $objResponse->purchaseOrderNo;
+
+			 	<?php
+/**
+ * @version $Id: securepayxml.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Processors - eWay XML
+ * @copyright 2006-2008 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+ * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+ */
+
+// Dont allow direct linking
+( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
+
+/**
+* AcctExp Component
+* @package AEC - Account Control Expiration - Membership Manager
+* @subpackage processor
+* @copyright 2006-2008 Copyright (C) David Deutsch
+* @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
+* @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
+**/
+
+class processor_securepay extends XMLprocessor
+{
+	function info()
+	{
+		$info = array();
+		$info['name']			= 'securepayxml';
+		$info['longname']		= _CFG_SECUREPAY_LONGNAME;
+		$info['statement']		= _CFG_SECUREPAY_STATEMENT;
+		$info['description']	= _CFG_SECUREPAY_DESCRIPTION;
+		$info['cc_list']		= 'visa,mastercard';
+		$info['recurring']		= 0;
+
+		return $info;
+	}
+
+	function settings()
+	{
+		$settings = array();
+		$settings['testmode']		= "1";
+		$settings['merchantid']		= '';
+		$settings['password']		= '';
+		$settings['tax']			= "10";
+		$settings['testAmount']		= "00";
+		$settings['item_name']		= sprintf( _CFG_PROCESSOR_ITEM_NAME_DEFAULT, '[[cms_live_site]]', '[[user_name]]', '[[user_username]]' );
+		$settings['rewriteInfo']	= '';
+		$settings['SiteTitle']		= '';
+
+		return $settings;
+	}
+
+	function backend_settings()
+	{
+		$settings = array();
+		$settings['testmode']		= array( 'list_yesno' );
+		$settings['merchantid']		= array( 'inputC' );
+		$settings['password']		= array( 'inputC' );
+		$settings['SiteTitle']		= array( 'inputC' );
+		$settings['item_name']		= array( 'inputE' );
+
+ 		$rewriteswitches			= array( 'cms', 'user', 'expiration', 'subscription', 'plan');
+		$settings = AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
+
+		return $settings;
+	}
+
+	function createRequestXML( $request )
+	{
+
+		$order_total = $request->int_var['amount'] * 100;
+
+		$xml .= '<?xml version="1.0" encoding="UTF-8"?>';
+		$xml .= '<SecurePayMessage>';
+
+		$xml .= '<MessageInfo>';
+		$xml .= '<messageID>'.$request->invoice->invoice_number.'</messageID>';
+		$xml .= '<messageTimestamp>'.date("YmdGis")."000+1000".'</messageTimestamp>';
+		$xml .= '<timeoutValue>60</timeoutValue>';
+		$xml .= '<apiVersion>xml-4.2</apiVersion>';
+		$xml .= '</MessageInfo>';
+
+		$xml .= '<MerchantInfo>';
+		$xml .= '<merchantID>xxxxxxxxxx</merchantID>';
+		$xml .= '<password>xxxxxxxxxx</password>';
+		$xml .= '</MerchantInfo>';
+
+		$xml .= '<RequestType>Payment</RequestType>';
+
+		$xml .= '<Payment>';
+		$xml .= '<TxnList count="1">';
+		$xml .= '<Txn ID="1">';
+		$xml .= '<txnType>0</txnType>';
+		$xml .= '<txnSource>0</txnSource>';
+		$xml .= '<amount>'.$order_total.'</amount>';
+		$xml .= '<purchaseOrderNo>'.$request->invoice->invoice_number.'</purchaseOrderNo>';
+
+		$xml .= '<CreditCardInfo>';
+		$xml .= '<cardNumber>'.$request->int_var['params']['cardNumber'].'</cardNumber>';
+		$xml .= '<expiryDate>'.$request->int_var['params']['expirationYear'].'/'.$request->int_var['params']['expirationMonth'].'</expiryDate>';
+		$xml .= '</CreditCardInfo>';
+
+		$xml .= '</Txn>';
+		$xml .= '</TxnList>';
+		$xml .= '</Payment>';
+
+		$xml .= '</SecurePayMessage>';
+
+		return $xml;
+	}
+/*SimpleXMLElement Object ( [MessageInfo] =>  SimpleXMLElement Object (
+[messageID] =>  IMGIzMzBiZTI1NDEx [messageTimestamp] =>
+20102907161629663000+600 [apiVersion] =>  xml-4.2 ) [RequestType] =>
+20102907161629663000+Payment
+[MerchantInfo] =>  SimpleXMLElement Object ( [merchantID] =>  XXXXXXXX
+) [Status] =>  SimpleXMLElement Object ( [statusCode] =>  000
+[statusDescription] =>  Normal ) [Payment] =>  SimpleXMLElement Object
+( [TxnList] =>  SimpleXMLElement Object ( [@attributes] =>  Array (
+[count] =>  1
+) [Txn] =>  SimpleXMLElement Object ( [@attributes] =>  Array ( [ID]
+=>  1 ) [txnType] =>  0 [txnSource] =>  0 [amount] =>  1 [currency] =>
+AUD [purchaseOrderNo] =>  IMGIzMzBiZTI1NDEx [approved] =>  No
+[responseCode] =>
+102 [responseText] =>  Invalid Expiry Date [thinlinkResponseCode] =>
+300 [thinlinkResponseText] =>  000 [thinlinkEventStatusCode] =>  981
+[thinlinkEventStatusText] =>  Error - Expired Card [settlementDate] =>
+SimpleXMLElement Object ( ) [txnID] =>  SimpleXMLElement Object ( )
+[CreditCardInfo] =>  SimpleXMLElement Object ( [pan] =>  444433...111
+[expiryDate] =>  SimpleXMLElement Object ( ) [cardType] =>  6
+[cardDescription] =>  Visa ) ) ) ) )*/
+	function transmitRequestXML( $xml, $request )
+	{
+		if ( $this->settings['testmode'] ) {
+			$url = 'https://test.securepay.com.au/xmlapi/payment';
+		} else {
+			$url = 'https://www.securepay.com.au/xmlapi/payment';
+		}
+
+		$response = array();
+
+		if ( $objResponse = simplexml_load_string( $this->transmitRequest( $url, '', $xml ) ) ) {
+			$response['amount_paid'] = $objResponse->amount / 100;
+			$response['invoice'] = $objResponse->purchaseOrderNo;
+
+			if ( $objResponse->Status->statusCode == '000' ) {
+				$response['valid'] = 1;
 			} else {
-				 $url = 'https://www.securepay.com.au/xmlapi/payment';
+				$response['valid'] = 0;
+				$response['error'] = 'Not processed';
 			}
+		} else {
+			$response['valid'] = 0;
+			$response['error'] = "Can't connect";
+		}
 
-			$response = array();
+		return $response;
+	}
 
-			/* I REALISE THE REST OF THIS PART NEEDS TO BE UPDATED TO REFLECT SECURE PAY'S RESPONSE XML
+	function checkoutform()
+	{
+		$var = $this->getUserform();
+		$var = $this->getCCform();
 
-			if ( $objResponse = simplexml_load_string( $this->transmitRequest( $url, '', $xml ) ) ) {
-				 $response['amount_paid'] = $objResponse->securepayReturnAmount / 100;
-				 $response['invoice'] = $objResponse->securepayTrxnOption2;
-
-				 if ( $objResponse->securepayTrxnStatus == 'True' ) {
-						$response['valid'] = 1;
-				 } else {
-						$response['valid'] = 0;
-						$response['error'] = $objResponse->securepayTrxnError;
-				 }
-			} else {
-				 $response['valid'] = 0;
-				 $response['error'] = _CFG_SECUREPAY_CONNECTION_ERROR;
-			}
-
-			return $response;
-								*/
-	 }
-
-	 function checkoutform()
-	 {
-			$var = $this->getUserform();
-			$var = $this->getCCform();
-
-			return $var;
-	 }
+		return $var;
+	}
 
 }
 ?>
