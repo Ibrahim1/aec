f30ed3544d1e8409aa7605b2284e8124c9181629
skore <skore@df06b4cb-4433-0410-b9f2-cc2e200029ab>
2010-07-31 08:46:54 +0000

    preparing toolbox push
    git-svn-id: http://joomlacode.org/svn/aec/trunk@3042 df06b4cb-4433-0410-b9f2-cc2e200029ab

--- newsrc/code/components/com_acctexp/toolbox/tool_supporthours.php
+++ newsrc/code/components/com_acctexp/toolbox/tool_supporthours.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @version $Id: tool_invoicecleanup.php
+ * @version $Id: tool_supporthours.php
  * @package AEC - Account Control Expiration - Membership Manager
- * @subpackage Toolbox - Invoice Cleanup
+ * @subpackage Toolbox - Support Hours
  * @copyright 2010 Copyright (C) David Deutsch
  * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
  * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
@@ -11,12 +11,12 @@
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
 
-class tool_invoicecleanup
+class tool_supporthours
 {
 	function Info()
 	{
 		$info = array();
-		$info['name'] = "Orphaned Subscription Cleanup";
+		$info['name'] = "Support Hours";
 		$info['desc'] = "Gets rid of subscriptions for users that don't exist anymore.";
 
 		return $info;
