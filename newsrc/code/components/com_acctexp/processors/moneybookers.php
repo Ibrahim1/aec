427f8f5ec916787410313a902dcf32ee54097734
David Deutsch <david@joomlatools.eu>
2010-10-27 20:29:43 +0200

    fix #200

--- newsrc/code/components/com_acctexp/processors/moneybookers.php
+++ newsrc/code/components/com_acctexp/processors/moneybookers.php
@@ -171,7 +171,7 @@ class processor_moneybookers extends POSTprocessor
 
 		$md5sig = md5( $post['merchant_id'] . $post['transaction_id'] . strtoupper( md5( $this->settings['secret_word'] ) ) . $post['mb_amount'] . $post['mb_currency'] . $post['status'] );
 
-		if ( ( $post['status'] == '2' ) && ( $md5sig == $post['md5sig'] ) ) {
+		if ( ( $post['status'] == '2' ) && ( strtoupper( $md5sig ) == $post['md5sig'] ) ) {
 			$response['valid'] = true;
 		}
 
