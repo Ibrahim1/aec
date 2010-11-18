0ea308c7b51f7450f6a807917e04fedb98988e73
daviddeutsch <david@timble.net>
2010-11-18 08:36:43 +0100

    re #217

--- newsrc/code/components/com_acctexp/toolbox/tool_cleanup.php
+++ newsrc/code/components/com_acctexp/toolbox/tool_cleanup.php
@@ -11,26 +11,53 @@
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
 
-class tool_invoicecleanup
+class tool_cleanup
 {
 	function Info()
 	{
 		$info = array();
 		$info['name'] = "System Cleanup";
-		$info['desc'] = "Gets rid of subscriptions for users that don't exist anymore.";
+		$info['desc'] = "Removes subscription data for users that have been deleted.";
 
 		return $info;
 	}
 
 	function Settings()
 	{
-		return array();
+		$settings = array();
+		$settings['delete']				= array( 'checkbox', 'Delete', 'Do the cleanup (this can delete a lot of data - do a check first)' );
+
+		return $settings;
 	}
 
 	function Action( $request )
 	{
-		// Find all memberships without a user account
-		// Delete them
+		$db = &JFactory::getDBO();
+
+		// Find all entries lacking an existing user account
+		$tables = array( 'cart', 'couponsxuser', 'invoices', 'metauser', 'subscr' );
+
+		$found = array( 'total' => 0 );
+		foreach ( $tables as $table ) {
+			$query = 'SELECT count(*)'
+					. ' FROM #__acctexp_' . $table . ' AS a'
+					. ' LEFT JOIN jos_users AS b ON a.userid = b.id'
+					. ' WHERE b.id is null'
+					;
+			$db->setQuery( $query );
+			$count = $db->loadResult();
+
+			$found[$table] = $count;
+			$found['total'] += $count;
+		}
+
+		if ( !empty( $_POST['delete'] ) ) {
+			
+		} else {
+
+
+			return 'Found a total of ' . $found['total'] . ' entries.';
+		}
 	}
 
 }
