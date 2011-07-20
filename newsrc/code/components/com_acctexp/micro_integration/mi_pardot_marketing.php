0a98f7c13cd22ed0b1c1bd24d65c1fde7d5731ea
David Deutsch <skore@valanx.org>
2011-07-20 03:39:57 +0200

    test #485 Cave Johnson, we're done here.

--- newsrc/code/components/com_acctexp/micro_integration/mi_pardot_marketing.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_pardot_marketing.php
@@ -25,12 +25,36 @@ class mi_pardot_marketing extends MI
 	function Settings()
 	{
         $settings = array();
-        $settings['url']			= array( 'inputE' );
-        $settings['query']			= array( 'inputD' );
+        $settings['email']			= array( 'inputC' );
+        $settings['password']		= array( 'inputC' );
+        $settings['user_key']		= array( 'inputC' );
+        $settings['pardot_lists']	= array( 'inputD' );
 
-        $settings					= $this->autoduplicatesettings( $settings, array(), true, true );
+		if ( !empty( $this->settings['email'] ) && !empty( $this->settings['password'] ) && !empty( $this->settings['user_key'] ) ) {
+			$db = &JFactory::getDBO();
+
+			$pc = new PardotConnector( $db );
+			$pc->get( $this->settings );
+
+        	$settings['api_key']		= array( 'p', null, null, "API Key currently in use: ".$pc->api_key );
+		} else {
+			$settings['api_key']		= array( 'p', null, null, "API Key currently in use: Please fill in the above details to request an API key" );
+		}
+
 
 		return $settings;
+	}
+
+	function checkInstallation()
+	{
+		$db = &JFactory::getDBO();
+
+		$app = JFactory::getApplication();
+
+		$tables	= array();
+		$tables	= $db->getTableList();
+
+		return in_array( $app->getCfg( 'dbprefix' ) . 'acctexp_mi_pardot_marketing', $tables );
 	}
 
 	function install()
@@ -39,42 +63,101 @@ class mi_pardot_marketing extends MI
 
 		$query = 'CREATE TABLE IF NOT EXISTS `#__acctexp_mi_pardot_marketing` ('
 		. '`id` int(11) NOT NULL auto_increment,'
-		. '`created_on` datetime NOT NULL default \'0000-00-00 00:00:00\''
+		. '`created_on` datetime NOT NULL default \'0000-00-00 00:00:00\','
 		. '`api_key` text NULL,'
 		. ' PRIMARY KEY (`id`)'
 		. ')'
 		;
 		$db->setQuery( $query );
 		$db->query();
+
 		return;
 	}
 
 	function relayAction( $request )
 	{
-		if ( !isset( $this->settings['url'.$request->area] ) ) {
+		if ( $request->action != 'action' ) {
 			return null;
 		}
 
-		$url = AECToolbox::rewriteEngineRQ( $this->settings['url'.$request->area], $request );
-		$query = AECToolbox::rewriteEngineRQ( $this->settings['query'.$request->area], $request );
+		$db = &JFactory::getDBO();
 
-		return $this->fetchURL( $this->createURL( $url, $query ) );
-	}
+		$pc = new PardotConnector( $db );
+		$pc->get( $this->settings );
 
-	function getAPIkey()
-	{
-		
+		return $pc->createUser( $this->settings, $request->metaUser->cmsUser->email );
 	}
 
 }
 
-class PardotConnector
+class PardotConnector extends serialParamDBTable
 {
+	/** @var int Primary key */
+	var $id					= null;
+	/** @var int */
+	var $created_on			= null;
+	/** @var int */
+	var $api_key			= null;
+
+	function PardotConnector( &$db )
+	{
+		parent::__construct( '#__acctexp_mi_pardot_marketing', 'id', $db );
+	}
+
+	function get( $settings )
+	{
+	 	$this->load(1);
+
+		if ( empty( $this->created_on ) || ( $this->created_on == '0000-00-00 00:00:00' ) ) {
+			global $aecConfig;
+
+			$db = &JFactory::getDBO();
+
+			$query = 'INSERT INTO #__acctexp_mi_pardot_marketing'
+			. ' VALUES( \'1\', \'' . date( 'Y-m-d H:i:s', ((int) gmdate('U')) ) . '\', \'\' )'
+			;
+			$db->setQuery( $query );
+			$db->query() or die( $db->stderr() );
+
+			$this->load(1);
+		}
+
+		$diff = strtotime( $this->created_on ) - ((int) gmdate('U'));
+
+		// Check whether key is null or old
+		if ( ( $diff > 3300 ) || empty( $this->api_key ) || ( $this->api_key == 'Login failed' ) ) {
+			$request = $this->getAPIkey( $settings );
+
+			if ( strpos( $request, 'stat="fail"' ) !== false ) {
+				$this->api_key = 'Login failed';
+			} else {
+				$this->api_key = XMLprocessor::XMLsubstring_tag( $request, 'api_key' );
+			}
+
+			$this->storeload();
+		}
+	}
+
 	function getAPIkey( $settings )
 	{
 		$params = array( 'email' => $settings['email'], 'password' => $settings['password'], 'user_key' => $settings['user_key'] );
 
 		return $this->fetch( 'login', null, $params );
+	}
+
+	function createUser( $settings, $email )
+	{
+		$params = array( 'user_key' => $settings['user_key'], 'api_key' => $this->api_key );
+
+		if ( !empty( $settings['pardot_lists'] ) ) {
+			$li = explode( "\n", $settings['pardot_lists'] );
+			
+			foreach ( $li as $k ) {
+				$params[$k] = "1";
+			}
+		}
+
+		return $this->fetch( 'prospect', 'do/create/email/'.$email, $params );
 	}
 
 	function fetch( $area, $cmd, $params )
@@ -97,7 +180,7 @@ class PardotConnector
 
 			$url .= implode( '&', $ps );
 		}
-
+aecDebug("calling Pardot: ".$url);
 		$url_parsed = parse_url( $url );
 
 		$host = $url_parsed["host"];
@@ -124,49 +207,8 @@ class PardotConnector
 		} else {
 			$response = processor::doTheHttp( $url, $path, '', $port );
 		}
-
-		return true;
-	}
-}
-
-class aec_pardot_marketing extends serialParamDBTable
-{
-	/** @var int Primary key */
-	var $id					= null;
-	/** @var int */
-	var $created_on			= null;
-	/** @var int */
-	var $api_key			= null;
-
-	function aec_pardot_marketing( &$db )
-	{
-		parent::__construct( '#__acctexp_mi_pardot_marketing', 'id', $db );
-	}
-
-	function get( $settings )
-	{
-	 	$this->load(1);
-
-		if ( empty( $this->created_on ) || ( $this->created_on == '0000-00-00 00:00:00' ) ) {
-			global $aecConfig;
-
-			$query = 'INSERT INTO #__acctexp_mi_pardot_marketing'
-			. ' VALUES( \'1\', \'' . date( 'Y-m-d H:i:s', ((int) gmdate('U')) ) . '\' )'
-			;
-			$db->setQuery( $query );
-			$db->query() or die( $db->stderr() );
-
-			$this->load(1);
-		}
-
-		$diff = strtotime( $this->created_on ) - ((int) gmdate('U'));
-
-		if ( $diff > 3300 ) {
-			// Request new API key
-			$pc = new PardotConnector();
-			
-			$this->api_key = $pc->getAPIkey( $settings );
-		}
+aecDebug($response);
+		return $response;
 	}
 }
 
