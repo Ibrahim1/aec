acb55d726e26b205d08546552db44c851204e177
David Deutsch <skore@valanx.org>
2011-10-10 02:44:41 +0200

    fixed #561

--- newsrc/code/components/com_acctexp/processors/sofortueberweisung.php
+++ newsrc/code/components/com_acctexp/processors/sofortueberweisung.php
@@ -31,14 +31,16 @@ class processor_sofortueberweisung extends POSTprocessor
 	function settings()
 	{
 		$settings = array();
-		$settings['user_id']			= '123456';
-		$settings['project_id']			= '123456';
-		$settings['project_password']	= '';
-		$settings['currency']			= 'EUR';
-		$settings['language']			= 'DE';
-		$settings['item_name_1']		= "";
-		$settings['item_name_2']		= "";
-		$settings['customparams']		= "";
+		$settings['user_id']				= '123456';
+		$settings['project_id']				= '123456';
+		$settings['project_password']		= '';
+		$settings['hash_encoding']			= 'SHA1';
+		$settings['notification_password']	= '';
+		$settings['currency']				= 'EUR';
+		$settings['language']				= 'DE';
+		$settings['item_name_1']			= "";
+		$settings['item_name_2']			= "";
+		$settings['customparams']			= "";
 
 		return $settings;
 	}
@@ -50,11 +52,25 @@ class processor_sofortueberweisung extends POSTprocessor
 		$settings['user_id']				= array( 'inputC' );
 		$settings['project_id']				= array( 'inputC' );
 		$settings['project_password']		= array( 'inputC' );
+		$settings['hash_encoding']			= array( 'list' );
+		$settings['notification_password']	= array( 'inputC' );
 		$settings['currency']				= array( 'list_currency' );
 		$settings['language']				= array( 'list_language' );
 		$settings['item_name_1']			= array( 'inputE' );
 		$settings['item_name_2']			= array( 'inputE' );
 		$settings['customparams']			= array( 'inputD' );
+
+ 		$encoding = array();
+		$encoding[] = JHTML::_('select.option', "MD5", "MD5" );
+		$encoding[] = JHTML::_('select.option', "SHA1", "SHA1" );
+
+		if ( !empty( $this->settings['hash_encoding'] ) ) {
+			$enc = $this->settings['hash_encoding'];
+		} else {
+			$enc = "SHA1";
+		}
+
+		$settings['lists']['hash_encoding']	= JHTML::_('select.genericlist', $encoding, 'hash_encoding', 'size="2"', 'value', 'text', $enc );
 
 		$settings = AECToolbox::rewriteEngineInfo( null, $settings );
 
@@ -69,8 +85,8 @@ class processor_sofortueberweisung extends POSTprocessor
 		$var['project_id']		= $this->settings['project_id'];
 		$var['amount']			= $request->int_var['amount'];
 
-		$var['reason_1']		= urlencode( AECToolbox::rewriteEngineRQ( $this->settings['item_name_1'], $request ) );
-		$var['reason_2']		= urlencode( AECToolbox::rewriteEngineRQ( $this->settings['item_name_2'], $request ) );
+		$var['reason_1']		= trim( substr( AECToolbox::rewriteEngineRQ( $this->settings['item_name_1'], $request ), 0, 27 ) );
+		$var['reason_2']		= trim( substr( AECToolbox::rewriteEngineRQ( $this->settings['item_name_2'], $request ), 0, 27 ) );
 
 		$var['user_variable_0']	= $request->invoice->invoice_number;
 
@@ -78,18 +94,19 @@ class processor_sofortueberweisung extends POSTprocessor
 		$var['currency_id']		= $this->settings['currency'];
 		$var['language_id']		= $this->settings['language'];
 
-		$var['hash'] =	sha1(
-								$var['user_id'] . '|'
-								. $var['project_id'] . '|'
-								. '||||'
-								. $var['amount'] . '|'
-								. $var['currency_id'] . '|'
-								. $var['reason_1'] . '|'
-								. $var['reason_2'] . '|'
-								. $var['user_variable_0'] . '|'
-								. '|||||'
-								. $this->settings['project_password']
-						);
+		$prehash =	$var['user_id'] . '|'
+					. $var['project_id'] . '|'
+					. '||||'
+					. $var['amount'] . '|'
+					. $var['currency_id'] . '|'
+					. $var['reason_1'] . '|'
+					. $var['reason_2'] . '|'
+					. $var['user_variable_0'] . '|'
+					. '|||||'
+					. $this->settings['project_password']
+					;
+
+		$var['hash'] =	$this->getHash( $prehash );
 
 		return $var;
 	}
@@ -112,14 +129,20 @@ class processor_sofortueberweisung extends POSTprocessor
 
 	function validateNotification( $response, $post, $invoice )
 	{
-		$values = array( 'transaction','user_id','project_id','sender_holder','sender_account_number',
-							'sender_bank_code','sender_bank_name','sender_bank_bic','sender_iban','sender_country_id',
-							'recipient_holder','recipient_account_number','recipient_bank_code','recipient_bank_name','recipient_bank_bic',
-							'recipient_iban','recipient_country_id','international_transaction','amount','currency_id',
-							'reason_1','reason_2','security_criteria','user_variable_0','user_variable_1',
-							'user_variable_2','user_variable_3','user_variable_4', 'user_variable_5','created');
 
-		if ( empty( $post['transaction'] ) ) {
+		$values = array(	'transaction','user_id','project_id',
+							'sender_holder','sender_account_number','sender_bank_code','sender_bank_name','sender_bank_bic','sender_iban','sender_country_id',
+							'recipient_holder','recipient_account_number','recipient_bank_code','recipient_bank_name','recipient_bank_bic',
+							'recipient_iban','recipient_country_id',
+							'international_transaction','amount','currency_id',
+							'reason_1','reason_2','security_criteria',
+							'user_variable_0','user_variable_1','user_variable_2','user_variable_3','user_variable_4', 'user_variable_5',
+							'created'
+						);
+
+		$getmode = empty( $post['transaction'] );
+
+		if ( $getmode ) {
 			$status = aecGetParam( 'status' );
 
 			if ( !empty( $status ) ) {
@@ -133,34 +156,43 @@ class processor_sofortueberweisung extends POSTprocessor
 			}
 		}
 
-		$array = array();
 		foreach ( $values as $value ) {
-			if ( empty( $post['transaction'] ) ) {
-				$array[] = aecGetParam( $value );
-			} else {
-				$array[] = $post[$value];
+			if ( $getmode ) {
+				$post[$value] = aecGetParam( $value );
 			}
+
+			$array[$value] = $post[$value];
 		}
 
-		$array[] = $this->settings['project_password'];
+		$array['notification_password'] = $this->settings['notification_password'];
 
-		$hash = md5( implode( '|', $array ) );
+		$hash = $this->getHash( implode( '|', $array ) );
 
 		$response['valid'] = 0;
 
-		if ( empty( $post['transaction'] ) ) {
+		if ( $getmode ) {
 			$ohash = aecGetParam( 'hash' );
 		} else {
 			$ohash = $post['hash'];
 		}
 
 		if ( empty( $ohash ) || ( $hash != $ohash ) ) {
-			$response['pending_reason'] = 'hash value verification failed';
+			$response['pending_reason'] = 'hash value verification failed: ' . $hash . ' != ' . $ohash;
 		} else {
 			$response['valid'] = 1;
 		}
 
 		return $response;
+	}
+
+	function getHash( $string )
+	{
+		if ( $this->settings['hash_encoding'] == 'SHA1' ) {
+			return sha1( $string );
+		} else {
+			return md5( $string );
+		}
+
 	}
 
 }
