9c1876655e935e50b4b1278800b4d1eabb9500d5
David Deutsch <skore@valanx.org>
2011-10-24 03:49:04 +0200

    test #480

--- newsrc/code/components/com_acctexp/micro_integration/mi_jomsocial.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_jomsocial.php
@@ -25,6 +25,16 @@ class mi_jomsocial
 	{
 		$db = &JFactory::getDBO();
 
+		$psettings['change_points']			= array( 'inputB' );
+
+		$psettings = $this->autoduplicatesettings( $psettings );
+
+		$xsettings = array();
+		$xsettings['aectab_discount']			= array( 'tab', 'Userpoints Discount', 'Userpoints Discount' );
+		$xsettings['checkout_discount']			= array( 'list_yesno' );
+		$xsettings['checkout_conversion']		= array( 'inputB' );
+		$xsettings['aectab_action']				= array( 'tab', 'Userpoints - Action', 'Userpoints - Action' );
+
 		$settings = array();
 		$settings['overwrite_existing']	= array( 'list_yesno' );
 		$settings['assign_group']		= array( 'inputC' );
@@ -53,7 +63,45 @@ class mi_jomsocial
 
 		$settings					= AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
 
+		return array_merge( $settings, $xsettings, $psettings );
+	}
+
+	function getMIform( $request )
+	{
+		$settings = array();
+
+		$points = $this->getPoints( $request->metaUser->userid );
+
+		if ( $this->settings['checkout_discount'] && $points ) {
+			if ( !empty( $request->invoice->currency ) ) {
+				$currency = $request->invoice->currency;
+			} else {
+				global $aecConfig;
+
+				$currency = $aecConfig->cfg['standard_currency'];
+			}
+			
+			$value = AECToolbox::formatAmount( $this->settings['checkout_conversion'], $currency );
+			$total = AECToolbox::formatAmount( ( $points * $this->settings['checkout_conversion'] ), $currency );
+
+			$settings['vat_desc'] = array( 'p', "", sprintf( JText::_('MI_MI_JOMSOCIALUSERPOINTS_CONVERSION_INFO'), $points, $value, $total ) );
+			$settings['use_points'] = array( 'inputC', JText::_('MI_MI_JOMSOCIALPOINTS_USE_POINTS_NAME'), JText::_('MI_MI_JOMSOCIALPOINTS_USE_POINTS_DESC'), '' );
+		}
+
 		return $settings;
+	}
+
+	function verifyMIform( $request )
+	{
+		$return = array();
+
+		$request->params['use_points'] = (int) $request->params['use_points'];
+
+		if ( $request->params['use_points'] > $this->getPoints( $request->metaUser->userid ) ) {
+			$return['error'] = "You don't have that many points";
+		}
+
+		return $return;
 	}
 
 	function relayAction( $request )
@@ -106,6 +154,28 @@ class mi_jomsocial
 				}
 			}
 		}
+
+		if ( $request->action == 'action' ) {
+			$params = $request->metaUser->meta->getMIParams( $request->parent->id, $request->plan->id );
+
+			if ( $params['use_points'] ) {
+				$points = -$params['use_points'];
+
+				$this->updatePoints( $request->metaUser->userid, $points, $request->invoice->invoice_number );
+
+				unset( $params['use_points'] );
+
+				$request->metaUser->meta->setMIParams( $request->parent->id, $request->plan->id, $params, true );
+
+				$request->metaUser->meta->storeload();
+			}
+		}
+
+		if ( empty( $this->settings['change_points'.$request->area] ) ) {
+			return null;
+		}
+
+		$this->updatePoints( $request->metaUser->userid, $this->settings['change_points'.$request->area], $request->invoice->invoice_number );
 	}
 
 	function setFields( $fields, $userid )
@@ -215,6 +285,54 @@ class mi_jomsocial
 			$db->setQuery( $query );
 			$db->query();
 		}
+	}
+
+	function invoice_item_cost( $request )
+	{
+		if ( $this->settings['checkout_discount'] && !empty( $this->settings['checkout_conversion'] ) && !empty( $request->params['use_points'] ) ) {
+			return $this->modifyPrice( $request );
+		} else {
+			return null;
+		}
+	}
+
+	function modifyPrice( $request )
+	{
+		$discount = $request->params['use_points'] * $this->settings['checkout_conversion'];
+
+		$original_price = $request->add['terms']->nextterm->renderTotal();
+
+		if ( $discount > $original_price ) {
+			$discount = $original_price;
+
+			$request->params['use_points'] = $discount / $this->settings['checkout_conversion'];
+		}
+
+		$request->add['terms']->nextterm->discount( $discount, null, $request->params['use_points'] . " Points" );
+
+		return true;
+	}
+
+	function getPoints( $userid )
+	{
+		$db	   =& JFactory::getDBO();
+
+		$query = "SELECT points FROM #__community_users WHERE `userid`='" . $userid . "'";
+		$db->setQuery( $query );
+
+		return $db->loadResult();
+	}
+
+	function updatePoints( $userid, $points, $comment )
+	{
+		$db	   =& JFactory::getDBO();
+
+		$query = 'UPDATE #__community_users'
+				. ' SET `points` = \'' . ( $aupUser->points + $points ) . '\''
+				. ' WHERE `userid` = \'' . $aupUser->userid . '\''
+				;
+		$db->setQuery( $query );
+		$db->query();
 	}
 }
 ?>
