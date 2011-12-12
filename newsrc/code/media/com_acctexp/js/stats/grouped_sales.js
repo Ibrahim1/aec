3c7481055c2c3fc0d2e905e81feac55f595e71c9
David Deutsch <skore@valanx.org>
2011-12-12 03:47:42 +0100

    re #331 preparing stack vis

--- newsrc/code/media/com_acctexp/js/stats/grouped_sales.js
+++ newsrc/code/media/com_acctexp/js/stats/grouped_sales.js
@@ -157,7 +157,7 @@ function vCharts() {
 
 }
 
-Cellular = function(canvas, dim, dat) {
+function Cellular(canvas, dim, dat) {
 	var z = dim,
 		day = d3.time.format("%w"),
 		week = d3.time.format("%U"),
@@ -176,7 +176,7 @@ Cellular = function(canvas, dim, dat) {
 		.data([numyear])
 		.attr("class", "year y-"+r_start.slice(0, 4)+" RdYlGn")
 		//.attr("transform", "translate(42," + ( 1 + ( d - r_start ) * h*2.2 ) + ")")
-		.style("opacity", "0.5");
+		.style("opacity", "1.0");
 
 	year.append("svg:text")
 		.attr("transform", "translate(-6," + z * 3.5 + ")rotate(-90)")
@@ -188,10 +188,9 @@ Cellular = function(canvas, dim, dat) {
 		.enter()
 		.append("svg:rect")
 		.attr("class", "day")
-		.attr("width", z)
-		.attr("height", z)
-		.attr("x", function(d) { return week(d) * z; })
-		.attr("y", function(d) { return day(d) * z; })
+		.attr("width", 1).attr("height", 1)
+		.attr("x", function(d) { return (week(d) * z)+z/2; })
+		.attr("y", function(d) { return (day(d) * z); })
 		.on("mouseover", function(){
 			d3.select(this)
 				.transition().ease("elastic").duration(500)
@@ -226,10 +225,20 @@ Cellular = function(canvas, dim, dat) {
 
 		this.selectAll("rect.day")
 			.attr("class", function(d) { dd = format(d); test = nsales[format(d)];return "day q" + ccolor(nsales[format(d)]) + "-9"; })
+			.attr("ry", z/3.33).attr("rx", z/3.33)
 			.append("svg:title")
-			.text(function(d) { return format(d) + (format(d) in nsales ? ": " + amount_format(nsales[format(d)]) + amount_currency : ""); });
+			.text(function(d) { return format(d) + (format(d) in nsales ? ": " + amount_format(nsales[format(d)]) + amount_currency : ""); })
 
-		this.transition().ease("cubic").delay(100).duration(500).style("opacity", "1.0")
+		this.selectAll("rect.day")
+			.transition().ease("bounce")
+			.delay(function(d, i) { return i * 4; })
+			.duration(600)
+			.attr("width", z).attr("height", z)
+			.attr("ry", 0).attr("rx", 0)
+			.attr("x", function(d) { return week(d) * z; })
+			.attr("y", function(d) { return day(d) * z; });
+
+		//this.transition().ease("cubic").delay(100).duration(500).style("opacity", "1.0")
 	});
 
 	function monthPath(t0) {
@@ -244,7 +253,7 @@ Cellular = function(canvas, dim, dat) {
 	}
 }
 
-Sunburst = function(canvas, dim, dat) {
+function Sunburst(canvas, dim, dat) {
 	var r = dim / 2,
 		format = d3.time.format("%Y-%m-%d %X"),
 		selector = "#"+canvas.attr("id");
@@ -296,11 +305,12 @@ Sunburst = function(canvas, dim, dat) {
 	pre.key = 0;
 	pre.values = d3.nest()
 		.key(function(d) { return d.group; })
-		.rollup(function(v) { return d3.nest()
-										.key(function(d) { return d.plan; })
-										.rollup(function(v) { return d3.sum(v.map(function(d) { return d.amount; })); })
-										.entries(v);
-							})
+		.rollup(function(v) {
+			return d3.nest()
+				.key(function(d) { return d.plan; })
+				.rollup(function(v) { return Math.max(d3.sum(v.map(function(d) { return d.amount; })), 0); })
+				.entries(v);
+		})
 		.entries(dat);
 
 	var path = canvas.data([pre]).selectAll("path")
@@ -312,39 +322,56 @@ Sunburst = function(canvas, dim, dat) {
 			.style("opacity", "0.8")
 			.style("stroke", "#fff")
 			.style("stroke-width", "0")
-			.style("fill", function(d) { return sunburst_color(( (typeof d.values != 'object') ? d.parent : d).key); })
-			.on("mouseover", function(d){
-				if (typeof d.values != 'object') {
-					name = "Plan " + d.key + ": ";
-					amount = amount_format(d.values) + amount_currency; 
-				} else {
-					name = "Group " + d.key + ": ";
-					amount = amount_format(d3.sum(d.values.map(function(v) { return v.values; }))) + amount_currency;
-				}
-				d3.select(this)
-					.transition().ease("cubic-out").duration(500)
-					.style("opacity", "1.0")
-					.style("stroke-width", "1");
-				d3.select(selector+" .console-amt")
-					.style("opacity", "0.2")
-					.text(name)
-					.transition().ease("cubic-out").duration(500)
-					.style("opacity", "1.0");
-				d3.select(selector+" .console-val")
-					.style("opacity", "0.2")
-					.text(amount)
-					.transition().ease("cubic-out").duration(500)
-					.style("opacity", "1.0");
-			})
-			.on("mouseout", function(){
-				d3.select(this)
-					.transition().ease("cubic-out").duration(500)
-					.style("opacity", "0.8")
-					.style("stroke-width", "0");
-	
-				center_console();
-			})
-			;
+			.style("fill", function(d) { return sunburst_color(( (typeof d.values != 'object') ? d.parent : d).key); });
+/*
+	path.transition()
+		.ease("bounce")
+		.duration(1000)
+		.attrTween("d", pathTween);
+
+	function pathTween(b){
+		var i = d3.interpolate(
+					{startAngle: function(d) { return d.x; }, endAngle: function(d) { return d.x; }},
+					{startAngle:function(d) { return d.x; }, endAngle:function(d) { return (d.x + d.dx)-0.01; }}
+					);
+
+		return function(t) {
+			return arc(i(t));
+		}
+	}
+*/
+	path.on("mouseover", function(d){
+			if (typeof d.values != 'object') {
+				name = "Plan " + d.key + ": ";
+				amount = amount_format(d.values) + amount_currency; 
+			} else {
+				name = "Group " + d.key + ": ";
+				amount = amount_format(d3.sum(d.values.map(function(v) { return v.values; }))) + amount_currency;
+			}
+			d3.select(this)
+				.transition().ease("cubic-out").duration(500)
+				.style("opacity", "1.0")
+				.style("stroke-width", "1");
+			d3.select(selector+" .console-amt")
+				.style("opacity", "0.2")
+				.text(name)
+				.transition().ease("cubic-out").duration(500)
+				.style("opacity", "1.0");
+			d3.select(selector+" .console-val")
+				.style("opacity", "0.2")
+				.text(amount)
+				.transition().ease("cubic-out").duration(500)
+				.style("opacity", "1.0");
+		})
+		.on("mouseout", function(){
+			d3.select(this)
+				.transition().ease("cubic-out").duration(500)
+				.style("opacity", "0.8")
+				.style("stroke-width", "0");
+
+			center_console();
+		})
+		;
 
 		var center_console = function() {
 		d3.select(selector+" .console-amt")
@@ -361,5 +388,167 @@ Sunburst = function(canvas, dim, dat) {
 	}
 
 	center_console();
+}
+
+function Stacked(canvas, dim, dat) {
+	var n = 2, // number of layers
+    m = 64, // number of samples per layer
+    color = d3.interpolateRgb("#aad", "#556");
+/*
+	var data = new Object;
+	data.key = 0;
+	data.values = d3.nest()
+		.key(function(d) { return d.date; })
+		.rollup(function(v) {
+			return d3.nest()
+			.key(function(d) { return d.group; })
+			.rollup(function(w) { return d3.nest()
+				.key(function(d) { return d.plan; })
+				.rollup(function(v) { return Math.max(d3.sum(w.map(function(d) { return d.amount; })), 0); })
+				.entries(w);
+				})
+			.entries(v);
+		})
+		.entries(dat);
+
+
+	var data = d3.layout.stack()(d3.range(n).map(function(dat) {
+	var a = [], i;
+	for (i = 0; i < m; i++) a[i] = .2 * Math.random();
+	for (i = 0; i < 5; i++) bump(a);
+	return a.map(stream_index);
+	}););
+*/
+	function stream_layers(n, m, o) {
+		  if (arguments.length < 3) o = 0;
+		  function bump(a) {
+		    var x = 1 / (.1 + Math.random()),
+		        y = 2 * Math.random() - .5,
+		        z = 10 / (.1 + Math.random());
+		    for (var i = 0; i < m; i++) {
+		      var w = (i / m - y) * z;
+		      a[i] += x * Math.exp(-w * w);
+		    }
+		  }
+		  return d3.range(n).map(function() {
+		      var a = [], i;
+		      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+		      for (i = 0; i < 5; i++) bump(a);
+		      return a.map(stream_index);
+		    });
+		}
+	function stream_index(d, i) {
+		  return {x: i, y: Math.max(0, d)};
+		}
+test = stream_layers(n,m);
+	var p = 20,
+    w = canvas.attr("width"),
+    h = canvas.attr("height") - .5 - p,
+    mx = m,
+    my = d3.max(data, function(d) {
+      return d3.max(d, function(d) {
+        return d.y0 + d.y;
+      });
+    }),
+    mz = d3.max(data, function(d) {
+      return d3.max(d, function(d) {
+        return d.y;
+      });
+    }),
+    x = function(d) { return d.x * w / mx; },
+    y0 = function(d) { return h - d.y0 * h / my; },
+    y1 = function(d) { return h - (d.y + d.y0) * h / my; },
+    y2 = function(d) { return d.y * h / mz; }; // or `my` to not rescale
+
+var layers = canvas.selectAll("g.layer")
+    .data(data)
+  .enter().append("g")
+    .style("fill", function(d, i) { return sunburst_color(i / (n - 1)); })
+    .attr("class", "layer");
+
+var bars = layers.selectAll("g.bar")
+    .data(function(d) { return d; })
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d) + ",0)"; });
+
+bars.append("rect")
+    .attr("width", x({x: .9}))
+    .attr("x", 0)
+    .attr("y", h)
+    .attr("height", 0)
+  .transition()
+    .delay(function(d, i) { return i * 10; })
+    .attr("y", y1)
+    .attr("height", function(d) { return y0(d) - y1(d); });
+/*
+var labels = canvas.selectAll("text.label")
+    .data(data[0])
+  .enter().append("text")
+    .attr("class", "label")
+    .attr("x", x)
+    .attr("y", h + 6)
+    .attr("dx", x({x: .45}))
+    .attr("dy", ".71em")
+    .attr("text-anchor", "middle")
+    .text(function(d, i) { return i; });
+*/
+canvas.append("line")
+    .attr("x1", 0)
+    .attr("x2", w - x({x: .1}))
+    .attr("y1", h)
+    .attr("y2", h);
+
+function transitionGroup() {
+  var group = d3.selectAll("#chart");
+
+  group.select("#group")
+      .attr("class", "first active");
+
+  group.select("#stack")
+      .attr("class", "last");
+
+  group.selectAll("g.layer rect")
+    .transition()
+      .duration(500)
+      .delay(function(d, i) { return (i % m) * 10; })
+      .attr("x", function(d, i) { return x({x: .9 * ~~(i / m) / n}); })
+      .attr("width", x({x: .9 / n}))
+      .each("end", transitionEnd);
+
+  function transitionEnd() {
+    d3.select(this)
+      .transition()
+        .duration(500)
+        .attr("y", function(d) { return h - y2(d); })
+        .attr("height", y2);
+  }
+}
+
+function transitionStack() {
+  var stack = d3.select("#chart");
+
+  stack.select("#group")
+      .attr("class", "first");
+
+  stack.select("#stack")
+      .attr("class", "last active");
+
+  stack.selectAll("g.layer rect")
+    .transition()
+      .duration(500)
+      .delay(function(d, i) { return (i % m) * 10; })
+      .attr("y", y1)
+      .attr("height", function(d) { return y0(d) - y1(d); })
+      .each("end", transitionEnd);
+
+  function transitionEnd() {
+    d3.select(this)
+      .transition()
+        .duration(500)
+        .attr("x", 0)
+        .attr("width", x({x: .9}));
+  }
+}
 
 }
