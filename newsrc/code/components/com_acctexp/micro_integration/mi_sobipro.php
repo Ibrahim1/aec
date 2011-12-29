9fa9b3657e5d29f752bd7a51b4019e53c22aa5a4
David Deutsch <skore@valanx.org>
2011-12-29 22:37:43 +0100

    fixed #487

--- newsrc/code/components/com_acctexp/micro_integration/mi_sobipro.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_sobipro.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * @version $Id: mi_sobi.php
+ * @version $Id: mi_sobipro.php
  * @package AEC - Account Control Expiration - Membership Manager
  * @subpackage Micro Integrations - Sigsiu Online Business Index
- * @copyright 2006-2010 Copyright (C) David Deutsch
+ * @copyright 2011 Copyright (C) David Deutsch
  * @author David Deutsch <skore@skore.de> & Team AEC - http://www.valanx.org
  * @license GNU/GPL v.2 http://www.gnu.org/licenses/old-licenses/gpl-2.0.html or, at your option, any later version
  */
@@ -42,35 +42,55 @@ class mi_sobipro extends MI
 	{
 		if ( isset( $this->settings['unpublish_all'.$request->area] ) ) {
 			if ( $this->settings['unpublish_all'.$request->area] ) {
-				$this->processItems( $request->metaUser, 0 );
+				$this->unpublishItems( $request->metaUser );
 			}
 		}
 
 		if ( isset( $this->settings['publish_all'.$request->area] ) ) {
 			if ( $this->settings['publish_all'.$request->area] ) {
-				$this->processItems( $request->metaUser, 1 );
+				$this->publishItems( $request->metaUser );
 			}
 		}
 
 		return true;
 	}
 
-	function processItems( $metaUser, $publishVar )
+	function publishItems( $metaUser )
 	{
-		$sobiPath = implode( DS, array( JPATH_ROOT, 'components', 'com_sobipro', 'lib', 'sobi.php' ) );
+		$db = &JFactory::getDBO();
 
-		if(!file_exists($sobiPath))
-		{
-			$this->setError( 'SobiPro cannot be found on this site! Path: ' . $sobiPath );
+		$query = 'UPDATE #__sobipro_object'
+				. ' SET `state` = \'1\''
+				. ' WHERE `owner` = \'' . $metaUser->userid . '\''
+				. ' AND `oType`=\'entry\''
+				;
+		$db->setQuery( $query );
+
+		if ( $db->query() ) {
+			return true;
+		} else {
+			$this->setError( $db->getErrorMsg() );
 			return false;
 		}
-		else
-		{
-			require_once( $sobiPath );
-			SPFactory::db()->update( 'spdb_object', array( 'state' => $publishVar ), array( 'owner' => $metaUser->userid ) );
+	}
+
+	function unpublishItems( $metaUser )
+	{
+		$db = &JFactory::getDBO();
+
+		$query = 'UPDATE #__sobipro_object'
+				. ' SET `state` = \'0\''
+				. ' WHERE `owner` = \'' . $metaUser->userid . '\''
+				. ' AND `oType`=\'entry\''
+				;
+		$db->setQuery( $query );
+
+		if ( $db->query() ) {
+			return true;
+		} else {
+			$this->setError( $db->getErrorMsg() );
+			return false;
 		}
-		
-		return true;
 	}
 }
 
