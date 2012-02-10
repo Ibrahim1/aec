81822e7996e326778b9d9221b5ffb37f058a6198
David Deutsch <skore@valanx.org>
2012-02-10 11:20:12 +0100

    re #674

--- newsrc/code/components/com_acctexp/tmpl/classic/hold/html.php
+++ newsrc/code/components/com_acctexp/tmpl/classic/hold/html.php
@@ -1,21 +1,15 @@
 <?php
-if ( $userid > 0 ) {
-	$metaUser = new metaUser( $userid );
-
-	if ( $metaUser->hasSubscription ) {
-		// Make sure this really is pending
-		if ( strcmp($metaUser->objSubscription->status, 'Hold') !== 0 ) {
-			return aecNotAuth();
-		}
-	}
-
-	$document=& JFactory::getDocument();
-
-	$document->setTitle( html_entity_decode( JText::_('HOLD_TITLE'), ENT_COMPAT, 'UTF-8' ) );
-
-	$frontend = new HTML_frontEnd();
-	$frontend->hold( $option, $metaUser );
-} else {
-	aecRedirect( AECToolbox::deadsureURL( 'index.php' ) );
+if ( $userid == 0 ) {
+	return aecRedirect( AECToolbox::deadsureURL( 'index.php' ) );
 }
+
+if ( $metaUser->hasSubscription ) {
+	// Make sure this really is pending
+	if ( strcmp($metaUser->objSubscription->status, 'Hold') !== 0 ) {
+		return aecNotAuth();
+	}
+}
+
+$tmpl->setTitle( JText::_('HOLD_TITLE') );
+
 ?>
