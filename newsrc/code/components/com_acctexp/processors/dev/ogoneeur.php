076950c5293828f730cb9ef3a0d6cc59d88b890b
David Deutsch <skore@valanx.org>
2012-03-08 17:36:07 +0100

    re #525

--- newsrc/code/components/com_acctexp/processors/dev/ogoneeur.php
+++ newsrc/code/components/com_acctexp/processors/dev/ogoneeur.php
@@ -3,7 +3,8 @@
  * @version $Id: ogone.php
  * @package AEC - Account Control Expiration - Membership Manager
  * @subpackage Processors - Ogone
- * @author Vijay Jawalapersad
+ * @copyright 2012 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@valanx.org> & Team AEC - http://www.valanx.org with help from
  * @license GNU/GPL v.3 http://www.gnu.org/licenses/gpl.html or, at your option, any later version
  */
 
