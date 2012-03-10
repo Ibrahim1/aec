695bfdd928b63c4708f883f80ab4c0709bfbac7f
David Deutsch <skore@valanx.org>
2012-03-10 15:58:50 +0100

    test #678 fixed rwEngine

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -34,7 +34,7 @@ $langlist = array(	'com_acctexp' => JPATH_SITE,
 aecLanguageHandler::loadList( $langlist );
 
 define( '_AEC_VERSION', '1.0beta' );
-define( '_AEC_REVISION', '4796' );
+define( '_AEC_REVISION', '4814' );
 
 if ( !class_exists( 'paramDBTable' ) ) {
 	include_once( JPATH_SITE . '/components/com_acctexp/lib/eucalib/eucalib.php' );
@@ -1034,7 +1034,7 @@ class metaUser
 
 		$query = 'SELECT `profile_key`, `profile_value`'
 				. ' FROM #__user_profiles'
-				. ' WHERE `user_id` = \'' . $this->data['metaUser']->userid . '\'';
+				. ' WHERE `user_id` = \'' . $this->userid . '\'';
 		$db->setQuery( $query );
 		$objects = $db->loadObjectList();
 
