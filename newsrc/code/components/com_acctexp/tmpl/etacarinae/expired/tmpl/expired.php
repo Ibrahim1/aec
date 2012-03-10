ad6ed6c7669f1d48dd409d0cf36e79acedd37bc7
David Deutsch <skore@valanx.org>
2012-03-10 01:55:44 +0100

    re #674 improving expired view

--- newsrc/code/components/com_acctexp/tmpl/etacarinae/expired/tmpl/expired.php
+++ newsrc/code/components/com_acctexp/tmpl/etacarinae/expired/tmpl/expired.php
@@ -15,31 +15,39 @@
 		<div class="componentheading"><?php echo JText::_('EXPIRED_TITLE') ?></div>
 		<h4><?php echo sprintf( JText::_('DEAR'), $metaUser->cmsUser->name ) ?></h4>
 		<p><?php echo JText::_( $is_trial ? 'EXPIRED_TRIAL' : 'EXPIRED' ) . $expiration ?></p>
-		<div class="alert alert-warning">
+		
 			<?php if ( $invoice ) { ?>
-				<p><?php echo JText::_('PENDING_OPENINVOICE')
-								. '&nbsp;'
-								. $tmpl->lnk( array(	'task' => 'repeatPayment',
-														'invoice' => $invoice,
-														'userid' => $metaUser->id
-														), JText::_('GOTO_CHECKOUT') ) ?>
-				</p>
-			<?php } ?>
-			<?php if ( $continue ) { ?>
-				<div id="renew_button">
-					<?php echo $tmpl->btn( array(	'task' => 'renewSubscription',
-											'userid' => $metaUser->userid,
-											'usage' => $metaUser->focusSubscription->plan,
-											'intro' => $intro
-											), JText::_('RENEW_BUTTON_CONTINUE') ) ?>
+				<div class="alert alert-danger">
+				<p><?php echo JText::_('PENDING_OPENINVOICE') ?></p>
+				<?php echo $tmpl->btn( array(	'task' => 'repeatPayment',
+												'invoice' => $invoice,
+												'userid' => $metaUser->id
+												), JText::_('GOTO_CHECKOUT_BTN'),
+												'btn btn-danger' ) ?>
 				</div>
 			<?php } ?>
-			<div id="renew_button">
-					<?php echo $tmpl->btn( array(	'task' => 'renewSubscription',
-											'userid' => $metaUser->userid,
-											'intro' => $intro
-											), JText::_('RENEW_BUTTON') ) ?>
+			<?php if ( $continue ) { ?>
+				<div class="alert alert-info">
+					<div id="continue-button">
+						<p><?php echo JText::_('CONTINUE_BTN_INFO') ?></p>
+						<?php echo $tmpl->btn( array(	'task' => 'renewSubscription',
+												'userid' => $metaUser->userid,
+												'usage' => $metaUser->focusSubscription->plan,
+												'intro' => $intro
+												), JText::_('RENEW_BUTTON_CONTINUE'),
+												'btn btn-info' ) ?>
+					</div>
+				</div>
+			<?php } ?>
+			<div class="well">
+				<div id="renew-button">
+						<p><?php echo JText::_('RENEW_BTN_INFO') ?></p>
+						<?php echo $tmpl->btn( array(	'task' => 'renewSubscription',
+												'userid' => $metaUser->userid,
+												'intro' => $intro
+												), JText::_('RENEW_BUTTON'),
+												'btn btn-success' ) ?>
+				</div>
 			</div>
-		</div>
 	</div>
 </div>
