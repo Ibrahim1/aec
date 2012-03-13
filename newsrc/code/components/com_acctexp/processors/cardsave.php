1488f3ff0554fac5f20910fccc927fc99be5a178
David Deutsch <skore@valanx.org>
2012-03-13 16:24:04 +0100

    re #688 trying some new encoding

--- newsrc/code/components/com_acctexp/processors/cardsave.php
+++ newsrc/code/components/com_acctexp/processors/cardsave.php
@@ -107,6 +107,10 @@ class processor_cardsave extends POSTprocessor
   		$var['ServerResultURL']		= $request->int_var['return_url'];
   		$var['PaymentFormDisplaysResult']	= 'false';
 
+		foreach ( $var as $k => $v ) {
+			$var[$k] = htmlentities($v);
+		}
+
 		return array_merge( array( 'post_url' => $url, 'HashDigest' => $this->createhash( $var ) ), $var );	
 	}
 
