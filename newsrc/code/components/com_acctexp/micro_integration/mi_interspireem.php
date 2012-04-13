d5225188d54a8136b69e1c8ef3bff163743a43b9
murielgrabert <murigra@aol.com>
2012-04-13 11:59:02 +0200

    re #711

--- newsrc/code/components/com_acctexp/micro_integration/mi_interspireem.php
+++ newsrc/code/components/com_acctexp/micro_integration/mi_interspireem.php
@@ -16,8 +16,8 @@ class mi_interspireem
 	function Info()
 	{
 		$info = array();
-		$info['name'] = JText::_('AEC_MI_NAME_interspireem');
-		$info['desc'] = JText::_('AEC_MI_DESC_interspireem');
+		$info['name'] = JText::_('AEC_MI_NAME_INTERSPIREEM');
+		$info['desc'] = JText::_('AEC_MI_DESC_INTERSPIREEM');
 
 		return $info;
 	}
@@ -27,14 +27,12 @@ class mi_interspireem
 		$li = array();
 		$li[] = JHTML::_('select.option', 0, "--- --- ---" );
 
-		if ( !empty( $this->settings['api_key'] ) ) {
-			$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-			$lists = $MCAPI->lists();
+		if ( !empty( $this->settings['user_token'] ) ) {
+			$lists = $this->GetLists();
 
 			if ( !empty( $lists ) ) {
 				foreach( $lists as $list ) {
-					$li[] = JHTML::_('select.option', $list['id'], $list['name'] );
+					$li[] = JHTML::_('select.option', $list->listid, $list->name );
 				}
 			}
 		}
@@ -47,23 +45,17 @@ class mi_interspireem
 			$this->settings['list_exp'] = 0;
 		}
 
-		$settings = array();
-		$settings['user_name']		= array( 'inputC' );
-		$settings['user_token']	= array( 'inputC' );
-
-		$settings['custom_details']	= array( 'inputD' );
-
 		$settings['lists']['list']				= JHTML::_( 'select.genericlist', $li, 'list', 'size="4"', 'value', 'text', $this->settings['list'] );
-		$settings['lists']['list_unsub']		= JHTML::_( 'select.genericlist', $li, 'list_unsub', 'size="4"', 'value', 'text', $this->settings['list_unsub'] );
 		$settings['lists']['list_exp']			= JHTML::_( 'select.genericlist', $li, 'list_exp', 'size="4"', 'value', 'text', $this->settings['list_exp'] );
-		$settings['lists']['list_exp_unsub']	= JHTML::_( 'select.genericlist', $li, 'list_exp_unsub', 'size="4"', 'value', 'text', $this->settings['list_exp_unsub'] );
 
-		$settings['list']			= array( 'list' );
-		$settings['list_unsub']		= array( 'list' );
-		$settings['list_exp']		= array( 'list' );
-		$settings['list_exp_unsub']	= array( 'list' );
-		$settings['user_checkbox']	= array( 'toggle' );
-		$settings['custominfo']		= array( 'inputD' );
+		$settings = array();
+		$settings['user_name']			= array( 'inputC' );
+		$settings['user_token']			= array( 'inputC' );
+
+		$settings['custom_details']		= array( 'inputD' );
+
+		$settings['list']				= array( 'list' );
+		$settings['list_exp']			= array( 'list' );
 
 		$rewriteswitches			= array( 'cms', 'user', 'expiration', 'subscription', 'plan', 'invoice' );
 		$settings					= AECToolbox::rewriteEngineInfo( $rewriteswitches, $settings );
@@ -71,170 +63,69 @@ class mi_interspireem
 		return $settings;
 	}
 
-	function Defaults()
-	{
-		$settings = array();
-
-		$settings['user_checkbox']		= 1;
-
-		return $settings;
-	}
-
-
-	function profile_info( $request )
-	{
-		if ( !empty( $this->settings['api_key'] ) && !empty( $this->settings['account_id'] ) && !empty( $this->settings['account_name'] ) )  {
-			$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-			$mcuser = $MCAPI->listMemberInfo( $this->settings['list'], $request->metaUser->cmsUser->email);
-
-			if ( empty( $mcuser['id'] ) ) {
-				$mcuser = $MCAPI->listMemberInfo( $this->settings['list_exp'], $request->metaUser->cmsUser->email);
-
-				$listid = $this->settings['list_exp'];
-			} else {
-				$listid = $this->settings['list'];
-			}
-
-			$server = explode( '-', $this->settings['api_key'] );
-
-			if ( !empty( $mcuser['id'] ) ) {
-				$message = '<p><a href="http://' . $this->settings['account_name'] . '.' . $server[1] . '.list-manage.com/unsubscribe?u=' . $this->settings['account_id'] . '&id=' . $listid . '">Unsubscribe from our newsletter</a></p>';
-				return $message;
-			}
-		}
-
-		return '';
-	}
-
-	function getMIform( $request )
-	{
-		$settings = array();
-
-		if ( empty( $this->settings['user_checkbox'] ) ) {
-			return $settings;
-		}
-
-		$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list'], $request->metaUser->cmsUser->email);
-
-		if ( empty( $mcuser['id'] ) ) {
-			if ( empty( $this->settings['user_checkbox'] ) ) {
-				return $settings;
-			}
-
-			if ( !empty( $this->settings['custominfo'] ) ) {
-				$settings['exp'] = array( 'p', "", $this->settings['custominfo'] );
-			} else {
-				$settings['exp'] = array( 'p', "", JText::_('MI_MI_interspireem_DEFAULT_NOTICE') );
-			}
-
-			$settings['get_newsletter'] = array( 'checkbox', 'mi_'.$this->id.'_get_newsletter', 1, 0, "Sign up to our Newsletter" );
-		}
-
-		return $settings;
-	}
-
 	function expiration_action( $request )
 	{
-		if ( empty( $this->settings['list_exp'] ) ) {
-			return null;
+		if ( !empty( $this->settings['list'] ) ) {
+			$this->DeleteSubscriber( $request, $this->settings['list'] );
 		}
 
-		$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-		$name = $request->metaUser->explodeName();
-
-		$merge_vars = array( 'FNAME' => $name['first'], 'LNAME' => $name['last'] );
-
-		$merge_vars = processor::customParams( $this->settings['custom_details'], $merge_vars, $request );
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list_exp'], $request->metaUser->cmsUser->email );
-
-		if ( empty( $mcuser['id'] ) ) {
-			$result = $MCAPI->listSubscribe( $this->settings['list_exp'], $request->metaUser->cmsUser->email, $merge_vars );
-		}
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list_exp_unsub'], $request->metaUser->cmsUser->email );
-
-		if ( !empty( $mcuser['id'] ) ) {
-			$result = $MCAPI->listUnsubscribe( $this->settings['list_exp_unsub'], $request->metaUser->cmsUser->email, false, false, false);
+		if ( !empty( $this->settings['list_exp'] ) ) {
+			$this->AddSubscriberToList( $request, $request->metaUser->cmsUser->email, $this->settings['list_exp'] );
 		}
 	}
 
 	function action( $request )
 	{
-		if ( empty( $this->settings['list'] ) ) {
-			return null;
-		}
-
-		$MCAPI = new MCAPI( $this->settings['api_key'] );
-
-		$is_allowed = false;
-
-		if ( empty( $this->settings['user_checkbox'] ) ) {
-			$is_allowed = true;
-		} elseif ( !empty( $this->settings['user_checkbox'] ) && !empty( $request->params['get_newsletter'] ) ) {
-			$is_allowed = true;
-		}
-
-		$name = $request->metaUser->explodeName();
-
-		$merge_vars = array( 'FNAME' => $name['first'], 'LNAME' => $name['last'] );
-
-		$merge_vars = processor::customParams( $this->settings['custom_details'], $merge_vars, $request );
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list'], $request->metaUser->cmsUser->email );
-
-		if ( empty( $mcuser['id'] ) && $is_allowed ) {
-			$result = $MCAPI->listSubscribe( $this->settings['list'], $request->metaUser->cmsUser->email, $merge_vars );
-		}
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list_unsub'], $request->metaUser->cmsUser->email );
-
-		if ( !empty( $mcuser['id'] ) ) {
-			$result = $MCAPI->listUnsubscribe( $this->settings['list_unsub'], $request->metaUser->cmsUser->email, false, false, false);
+		if ( !empty( $this->settings['list'] ) ) {
+			$this->AddSubscriberToList( $request, $request->metaUser->cmsUser->email, $this->settings['list'] );
 		}
 	}
 
 	function on_userchange_action( $request )
 	{
-		$MCAPI = new MCAPI( $this->settings['api_key'] );
+		if ( !empty( $this->settings['list'] ) ) {
+			$this->DeleteSubscriber( $request, $request->metaUser->cmsUser->email, $this->settings['list'] );
+		}
 
-		$email = $request->row->email;
-
-		$mcuser = $MCAPI->listMemberInfo( $this->settings['list'], $email );
-
-		$request->metaUser = new metaUser( $request->row->id );
-
-		$country = AECToolbox::rewriteEngineRQ( $this->settings['country' . $request->area], $request );
-
-		if ( $mcuser['id'] && ($request->trace == 'user' || $request->trace == 'adminuser') ) {
-			// if the name was submitted in the last request, we can use that name.  Otherwise, use the current name
-			if ( $request->post['name'] ) {
-				$name = metaUser::_explodeName( $request->post['name'] );
-			} else {
-				$name = $request->metaUser->explodeName();
-			}
-
-			$merge_vars = array( 'FNAME' => $name['first'], 'LNAME' => $name['last'] );
-
-			$merge_vars = processor::customParams( $this->settings['custom_details'], $merge_vars, $request );
-
-			$result = $MCAPI->listUpdateMember( $this->settings['list'], $email, $merge_vars, '', false );			
+		if ( !empty( $this->settings['list'] ) ) {
+			$this->AddSubscriberToList( $request, $request->post['email'], $this->settings['list'] );
 		}
 	}
-	
-	function createUser( $request, $list )
+
+	function DeleteSubscriber( $request, $list )
 	{
-		$xml = '<xmlrequest>
-				<username>' . $this->settings['user_name'] . '</username>
-				<usertoken>' . $this->settings['user_token'] . '</usertoken>
-				<requesttype>subscribers</requesttype>
-				<requestmethod>AddSubscriberToList</requestmethod>
-				<details>
-				<emailaddress>' . $request->metaUser->cmsUser->email . '</emailaddress>
+		$data = '<emailaddress>' . $request->metaUser->cmsUser->email . '</emailaddress>
+				<list>' . $list . '</list>';
+
+		$xml = $this->getRequest( 'subscribers' ,'DeleteSubscriber' , $data );
+
+		$result = $this->sendRequest( $xml );
+	}
+	
+	function GetLists()
+	{
+		$xml = $this->getRequest( 'user' ,'GetLists' , '' );
+
+		$result = $this->sendRequest( $xml );
+
+		$xml_doc = simplexml_load_string( $result );
+		
+		return $xml_doc->data;
+	}
+
+	function IsContactOnList( $request, $list )
+	{
+		$data = '<emailaddress>' . $request->metaUser->cmsUser->email . '</emailaddress>
+				<list>' . $list . '</list>';
+
+		$xml = $this->getRequest( 'subscribers' ,'IsContactOnList' , $data );
+
+		$result = $this->sendRequest( $xml );
+	}
+
+	function AddSubscriberToList( $request, $email, $list )
+	{
+		$data = '<emailaddress>' . $email . '</emailaddress>
 				<mailinglist>' . $list . '</mailinglist>
 				<format>html</format>
 				<confirmed>yes</confirmed>
@@ -243,28 +134,48 @@ class mi_interspireem
 				<fieldid>1</fieldid>
 				<value>John Smith</value>
 				</item>
-				</customfields>
-				</details>
+				</customfields>';
+
+		$xml = $this->getRequest( 'subscribers', 'AddSubscriberToList', $data );
+
+		$result = $this->sendRequest( $xml );
+	}
+
+	function GetCustomFieldData()
+	{
+		$data = '<listids>' . $this->settings['list'] . '</listids>';
+
+		$xml = $this->getRequest( 'lists', 'GetCustomFields<', $data );
+
+		$result = $this->sendRequest( $xml );
+	}
+
+	function getRequest($type, $method, $data )
+	{
+		$xml = '<xmlrequest>
+				<username>' . $this->settings['user_name'] . '</username>
+				<usertoken>' . $this->settings['user_token'] . '</usertoken>
+				<requesttype>' . $type . '</requesttype>
+				<requestmethod>' . $method . '</requestmethod>
+				<details>' . $data . '</details>
 				</xmlrequest>
 				';
-		$ch = curl_init( 'http://www.yourdomain.com/IEM/xml.php' );
-		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-		curl_setopt( $ch, CURLOPT_POST, 1 );
-		curl_setopt( $ch, CURLOPT_POSTFIELDS, $xml );
-		$result = @curl_exec( $ch );
-		if( $result === false ) {
-			echo "Error performing request";
-		} else {
-			$xml_doc = simplexml_load_string( $result );
-			echo 'Status is ', $xml_doc->status, '<br/>';
-			if ( $xml_doc->status == 'SUCCESS' ) {
-				echo 'Data is ', $xml_doc->data, '<br/>';
-			} else {
-				echo 'Error is ', $xml_doc->errormessage, '<br/>';
-			}
-		}
+
+		return $xml;
 	}
-		
+
+	function sendRequest( $xml )
+	{
+		$db = &JFactory::getDBO();
+
+		$path = '/IEM/xml.php';
+
+		$url = 'http://www.yourdomain.com' . $path;
+
+		$tempprocessor = new processor($db);
+
+		return $tempprocessor->transmitRequest( $url, $path, $xml );
+	}
 }
 
 ?>
