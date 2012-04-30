1920862aa214e76eff79109322539ca53c4c56d5
David Deutsch <skore@valanx.org>
2012-04-30 19:38:40 +0200

    fixed readout only showing one plan

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -34,7 +34,7 @@ $langlist = array(	'com_acctexp' => JPATH_SITE,
 aecLanguageHandler::loadList( $langlist );
 
 define( '_AEC_VERSION', '1.0beta' );
-define( '_AEC_REVISION', '5057' );
+define( '_AEC_REVISION', '5065' );
 
 if ( !class_exists( 'paramDBTable' ) ) {
 	include_once( JPATH_SITE . '/components/com_acctexp/lib/eucalib/eucalib.php' );
@@ -7754,7 +7754,7 @@ class SubscriptionPlanHandler
 
 		$query = 'SELECT id'
 			 	. ' FROM #__acctexp_plans'
-			 	. ' GROUP BY ' . ( $use_order ? '`ordering`' : '`id`' )
+			 	. ' ORDER BY ' . ( $use_order ? '`ordering`' : '`id`' )
 			 	;
 
 		if ( !empty( $limitstart ) && !empty( $limit ) ) {
