3136082ccabe8112eb126dc43be5c265f29d5118
David Deutsch <skore@valanx.org>
2012-05-06 17:15:47 +0200

    fixed #760

--- newsrc/code/components/com_acctexp/processors/paystation_direct.php
+++ newsrc/code/components/com_acctexp/processors/paystation_direct.php
@@ -2,7 +2,7 @@
 /**
  * @version $Id: paystation_direct.php
  * @package AEC - Account Control Expiration - Membership Manager
- * @subpackage Processors - paystation_direct
+ * @subpackage Processors - Paystation Direct
  * @copyright 2007-2012 Copyright (C) David Deutsch
  * @author David Deutsch <skore@valanx.org> & Team AEC - http://www.valanx.org
  * @license GNU/GPL v.3 http://www.gnu.org/licenses/gpl.html or, at your option, any later version
