9e1879093b587d63f52818fefe49a57bdb159b7c
David Deutsch <skore@valanx.org>
2012-07-16 12:39:42 +0200

    re #70 ready for testing

--- newsrc/code/modules/mod_acctexp/modules/mod_acctexp/mod_acctexp.php
+++ newsrc/code/modules/mod_acctexp/modules/mod_acctexp/mod_acctexp.php
@@ -11,122 +11,25 @@
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
 
-require_once( JApplicationHelper::getPath( 'class', 'com_acctexp' ) );
-
-$class_sfx				= $params->get( 'moduleclass_sfx', "");
-$pretext 				= $params->get( 'pretext' );
-$posttext 				= $params->get( 'posttext' );
-$showExpiration 		= $params->def( 'show_expiration', 0 );
-$displaypipeline		= $params->get( 'displaypipeline', 0 );
-$extended				= $params->get( 'extended', 0 );
-
 $user = &JFactory::getUser();
 
 if ( $user->id ) {
+	require_once( JApplicationHelper::getPath( 'class', 'com_acctexp' ) );
+	require_once dirname(__FILE__).'/helper.php';
+
+	$class_sfx				= $params->get( 'moduleclass_sfx', "");
+	$pretext 				= $params->get( 'pretext' );
+	$posttext 				= $params->get( 'posttext' );
+	$showExpiration 		= $params->def( 'show_expiration', 0 );
+	$displaypipeline		= $params->get( 'displaypipeline', 0 );
+
 	$lang =& JFactory::getLanguage();
 
 	$lang->load( 'mod_acctexp', JPATH_SITE );
 
-	echo '<div class="aec_module_inner' . $class_sfx . '">';
-
-	if ( !empty( $pretext ) ) {
-		echo $pretext;
-	}
-
-	if ( $showExpiration ) {
-		if ( !$extended ) {
-			echo AECModuleHelper::getExpirationSimple( $user );
-		} else {
-			$metaUser = new metaUser( $user->id );
-
-			$subscriptions = $metaUser->getAllCurrentSubscriptionsInfo();
-
-			foreach ( $subscriptions as $subscription ) {
-				echo '<div class="aec_module_subscription aec_module_subscriptionid_' . $subscription->plan  . '">';
-
-				echo "<h4>" . $subscription->name . "</h4>";
-
-				echo AECModuleHelper::textExpiration( $user, $subscription->expiration, $subscription->lifetime, $subscription->recurring );
-
-				echo '</div>';
-			}
-		}
-
-	}
-
-	if ( $displaypipeline ) {
-		$dph = new displayPipelineHandler;
-		echo $dph->getUserPipelineEvents( $user->id );
-	}
-
-	if ( !empty( $posttext ) ) {
-		echo $posttext;
-	}
-
-	echo '</div>';
+	require JModuleHelper::getLayoutPath('mod_acctexp', $params->get('layout', 'default'));
 }
 
-class AECModuleHelper
-{
-	function getExpirationSimple( $user )
-	{
-		$db = &JFactory::getDBO();
 
-		$expiration = null;
-		$query = 'SELECT `id`, `expiration`, `lifetime`, `recurring`'
-				. ' FROM #__acctexp_subscr'
-				. ' WHERE `userid` = \'' . $user->id . '\''
-				. ' AND `primary` = \'1\''
-				. ' AND `status` != \'Excluded\'';
-		$db->setQuery($query);
-		$entry = $db->loadObject();
-
-		if ( !empty( $entry->lifetime ) ) {
-			return AECModuleHelper::textExpiration( $user, $entry->expiration, $entry->lifetime, $entry->recurring );
-		}
-
-		if ( empty( $entry->expiration ) ) {
-			$query = 'SELECT `id`, `expiration`, `recurring`'
-					. ' FROM #__acctexp_subscr'
-					. ' WHERE `userid` = \'' . $user->id . '\''
-	                . ' AND `status` != \'Excluded\'';
-			$db->setQuery($query);
-			$entry = $db->loadObject();
-		}
-
-		if ( empty( $entry->id ) ) {
-			return null;
-		} else {
-			return AECModuleHelper::textExpiration( $user, $entry->expiration, $entry->lifetime, $entry->recurring );
-		}
-	}
-
-	function textExpiration( $user, $expiration, $lifetime=null, $recurring=false )
-	{
-		if ( empty( $expiration ) || $lifetime ) {
-			return AECModuleHelper::textUnlimited();
-		} else {
-			return AECModuleHelper::textExpirationDate( $expiration, $recurring );
-		}
-	}
-
-	function textUnlimited()
-	{
-		return "<p>" . JText::_('ACCOUNT_UNLIMIT') . "</p>";
-	}
-
-	function textExpirationDate( $expiration, $recurring )
-	{
-		global $aecConfig;
-
-		$retVal = AECToolbox::formatDate( ( strtotime( $expiration ) ) );
-
-		if ( $recurring ) {
-			return "<p>" . JText::_('ACCOUNT_RENEWAL') . ": " . $retVal . "</p>";
-		} else {
-			return "<p>" . JText::_('ACCOUNT_EXPIRES') . ": " . $retVal . "</p>";
-		}
-	}
-}
 
 ?>
--- newsrc/code/modules/mod_acctexp_cart/modules/mod_acctexp_cart/mod_acctexp_cart.php
+++ newsrc/code/modules/mod_acctexp_cart/modules/mod_acctexp_cart/mod_acctexp_cart.php
@@ -26,12 +26,6 @@ if ( $user->id ) {
 
 	$lang->load( 'mod_acctexp_cart', JPATH_SITE );
 
-	echo '<div class="aec_cart_module_inner' . $class_sfx . '">';
-
-	if ( !empty( $pretext ) ) {
-		echo $pretext;
-	}
-
 	$c = aecCartHelper::getCartbyUserid( $user->id );
 
 	$metaUser = new metaUser( $user->id );
@@ -39,7 +33,7 @@ if ( $user->id ) {
 	$cart = $c->getCheckout( $metaUser );
 
 	if ( empty( $c->content ) ) {
-		?><p><?php echo JText::_('AEC_CART_MODULE_CART_EMPTY'); ?></p><?php
+		require JModuleHelper::getLayoutPath('mod_acctexp', $params->get('layout', 'empty'));
 	} else {
 		switch ( $mode ) {
 			default:
@@ -53,52 +47,14 @@ if ( $user->id ) {
 						$total = $bitem['cost_total'];
 					}
 				}
-				?><p><?php echo JText::_('AEC_CART_MODULE_ITEMS_IN_CART'); ?>: <?php echo $quantity; ?></p>
-				<p><?php echo JText::_('AEC_CART_MODULE_ROW_TOTAL'); ?>: <?php echo $total; ?></p>
-				<?php
+
+				require JModuleHelper::getLayoutPath('mod_acctexp', $params->get('layout', 'default'));
 				break;
 			case 'full':
-				?><table>
-					<tr>
-						<th><?php echo JText::_('AEC_CART_MODULE_ROW_ITEM'); ?></th>
-						<th><?php echo JText::_('AEC_CART_MODULE_ROW_COST'); ?></th>
-						<th><?php echo JText::_('AEC_CART_MODULE_ROW_AMOUNT'); ?></th>
-						<th><?php echo JText::_('AEC_CART_MODULE_ROW_TOTAL'); ?></th>
-					</tr><?php
-
-					foreach ( $cart as $bid => $bitem ) {
-						if ( !empty( $bitem['obj'] ) ) {
-							?><tr>
-								<td><?php echo $bitem['name']; ?></td>
-								<td><?php echo $bitem['cost']; ?></td>
-								<td><?php echo $bitem['quantity']; ?></td>
-								<td><?php echo $bitem['cost_total']; ?></td>
-							</tr><?php
-						} else {
-							?><tr>
-								<td><strong><?php echo JText::_('AEC_CART_MODULE_ROW_TOTAL'); ?></strong></td>
-								<td></td>
-								<td></td>
-								<td><strong><?php echo $bitem['cost_total']; ?></strong></td>
-							</tr><?php
-						}
-					}
-				?></table><?php
+				require JModuleHelper::getLayoutPath('mod_acctexp', $params->get('layout', 'full'));
 				break;
 		}
 	}
-
-	if ( $button ) {
-		global $aecConfig;
-
-		?><a href="<?php echo AECToolbox::deadsureURL( 'index.php?option=com_acctexp&task=cart', $aecConfig->cfg['ssl_signup'] ); ?>"><img src="<?php echo JURI::root() . 'media/com_acctexp/images/site/your_cart_button.png'; ?>" /></a><?php
-	}
-
-	if ( !empty( $posttext ) ) {
-		echo $posttext;
-	}
-
-	echo '</div>';
 }
 
 ?>
