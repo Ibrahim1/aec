478d348e65ebe12c4dbb1934719f648ef1b7f94b
David Deutsch <skore@valanx.org>
2012-08-07 16:29:37 +0200

    some incorrect dates

--- newsrc/code/components/com_acctexp/processors/airtoy.php
+++ newsrc/code/components/com_acctexp/processors/airtoy.php
@@ -3,7 +3,7 @@
  * @version $Id: airtoy.php
  * @package AEC - Account Control Expiration - Membership Manager
  * @subpackage Processors - Airtoy
- * @copyright 2004-2011-2012 Copyright (C) David Deutsch
+ * @copyright 2004-2012 Copyright (C) David Deutsch
  * @author David Deutsch <skore@valanx.org> & Team AEC - http://www.valanx.org
  * @license GNU/GPL v.3 http://www.gnu.org/licenses/gpl.html or, at your option, any later version
  */
