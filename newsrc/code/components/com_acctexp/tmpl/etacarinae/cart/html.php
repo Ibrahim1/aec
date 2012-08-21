0eb0b250da9d67edf565223177210b4d8517dab3
David Deutsch <skore@valanx.org>
2012-08-21 13:11:33 +0200

    fixes #89

---
+++ newsrc/code/components/com_acctexp/tmpl/etacarinae/cart/html.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @version $Id: cart/html.php
+ * @package AEC - Account Control Expiration - Membership Manager
+ * @subpackage Main Frontend
+ * @copyright 2012 Copyright (C) David Deutsch
+ * @author David Deutsch <skore@valanx.org> & Team AEC - http://www.valanx.org
+ * @license GNU/GPL v.3 http://www.gnu.org/licenses/gpl.html or, at your option, any later version
+ */
+
+// Dont allow direct linking
+( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' );
+
+if ( !empty( $tmpl->cfg['tos'] ) ) {
+	$js = 'function submitPayment() {
+		if ( document.confirmForm.tos.checked ) {
+			document.confirmForm.submit();
+		} else {
+			alert("' . html_entity_decode( JText::_('CONFIRM_TOS_ERROR') ) . ' )");
+		}
+	}';
+
+	$tmpl->addScriptDeclaration( $js );
+}
+
+
+if ( !empty( $tmpl->cfg['tos'] ) ) {
+	$tmpl->addScript( JURI::root(true).'/media/com_acctexp/js/jquery/jquery-1.7.2.min.js' );
+	$tmpl->addScript( JURI::root(true).'/media/com_acctexp/js/jquery/jquerync.js' );
+
+	$js = '
+jQuery(document).ready(function(jQuery) {
+	jQuery("button#confirmation").attr("disabled", "disabled");
+
+	jQuery("input#aec-tos").click( function(event) {
+		if ( this.checked ) {
+			jQuery("button#confirmation").removeAttr("disabled");
+		} else {
+			jQuery("button#confirmation").attr("disabled", "disabled");
+		}
+	});
+
+	jQuery("div#confirmation-button").hover( function(event) {
+		if ( !jQuery("input#aec-tos").is(":checked") ) {
+			jQuery("div#confirmation-tos").toggleClass("well-highlight");
+		}
+	});
+
+	jQuery("form#form-continue :submit").click( function(event) {
+			event.preventDefault();
+
+			if ( jQuery("form#form-confirm .aec-tax").length > 0) {
+				if ( jQuery("select.aec-tax").val() == "0" ) {
+					jQuery("div#confirmation-extra>div").addClass("alert-danger").removeClass("alert-success");
+
+					return;
+				}
+			}
+
+			if ( jQuery("input#aec-tos").is(\':checked\') ) {
+				jQuery("form#form-confirm").submit();
+			} else {
+				alert("' . html_entity_decode( JText::_('CONFIRM_TOS_ERROR') ) . '");
+			}
+	});
+});
+';
+
+	$tmpl->addScriptDeclaration( $js );
+}
+
+
+$tmpl->setTitle( JText::_('CART_TITLE') );
+
+$tmpl->addDefaultCSS();
+
+@include( $tmpl->tmpl( 'cart' ) );
--- newsrc/code/components/com_acctexp/tmpl/etacarinae/cart/tmpl/btn.php
+++ newsrc/code/components/com_acctexp/tmpl/etacarinae/cart/tmpl/btn.php
@@ -10,32 +10,29 @@
 
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' ) ?>
-<div class="well">
-	<div id="confirmation-button">
-		<p><?php echo JText::_('CONFIRM_INFO') ?></p>
-		<input type="hidden" name="option" value="<?php echo $option ?>" />
-		<input type="hidden" name="userid" value="<?php echo $userid ? $userid : 0 ?>" />
-		<input type="hidden" name="task" value="confirmCart" />
-		<input type="hidden" name="usage" value="<?php echo $InvoiceFactory->usage ?>" />
-		<input type="hidden" name="processor" value="<?php echo $InvoiceFactory->processor ?>" />
-		<?php if ( isset( $InvoiceFactory->recurring ) ) { ?>
-			<input type="hidden" name="recurring" value="<?php echo $InvoiceFactory->recurring ?>" />
-		<?php } ?>
-		<?php if ( $passthrough != false ) { ?>
-			<input type="hidden" name="aec_passthrough" value="<?php echo $passthrough ?>" />
-		<?php } ?>
-		<?php if ( !empty( $tmpl->cfg['tos_iframe'] ) && !empty( $tmpl->cfg['tos'] ) ) { ?>
-			<iframe src="<?php echo $tmpl->cfg['tos'] ?>" width="100%" height="150px"></iframe>
-			<p><input name="tos" type="checkbox" /><?php echo JText::_('CONFIRM_TOS_IFRAME') ?></p>
-			<button type="submit" onClick="javascript:submitPayment()" class="button btn btn-success"><i class="icon-ok icon-white"></i><?php echo JText::_('BUTTON_CONFIRM') ?></button>
-			<?php
-		} elseif ( !empty( $tmpl->cfg['tos'] ) ) { ?>
-			<p><input name="tos" type="checkbox" /><?php echo JText::sprintf( 'CONFIRM_TOS', $tmpl->cfg['tos'] ) ?></p>
-			<button type="submit" onClick="javascript:submitPayment()" class="button btn btn-success"><i class="icon-ok icon-white"></i><?php echo JText::_('BUTTON_CONFIRM') ?></button>
-			<?php
-		} else { ?>
-			<button type="submit" class="button btn btn-success"><i class="icon-ok icon-white"></i><?php echo JText::_('BUTTON_CONFIRM') ?></button>
-			<?php
-		} ?>
+<?php if ( !empty( $tmpl->cfg['tos_iframe'] ) && !empty( $tmpl->cfg['tos'] ) ) { ?>
+	<div class="well" id="confirmation-tos">
+		<iframe src="<?php echo $tmpl->cfg['tos'] ?>" width="100%" height="150px"></iframe>
+		<p><input name="tos" type="checkbox" id="aec-tos" />&nbsp;&nbsp;<?php echo JText::_('CONFIRM_TOS_IFRAME') ?></p>
 	</div>
+<?php } elseif ( !empty( $tmpl->cfg['tos'] ) ) { ?>
+	<div class="well" id="confirmation-tos">
+		<p><input name="tos" type="checkbox" id="aec-tos" />&nbsp;&nbsp;<?php echo JText::sprintf( 'CONFIRM_TOS', $tmpl->cfg['tos'] ) ?></p>
+	</div>
+<?php } ?>
+
+<div class="well" id="confirmation-button">
+	<p><?php echo JText::_('CONFIRM_INFO') ?></p>
+	<input type="hidden" name="option" value="<?php echo $option ?>" />
+	<input type="hidden" name="userid" value="<?php echo $userid ? $userid : 0 ?>" />
+	<input type="hidden" name="task" value="confirmCart" />
+	<input type="hidden" name="usage" value="<?php echo $InvoiceFactory->usage ?>" />
+	<input type="hidden" name="processor" value="<?php echo $InvoiceFactory->processor ?>" />
+	<?php if ( isset( $InvoiceFactory->recurring ) ) { ?>
+		<input type="hidden" name="recurring" value="<?php echo $InvoiceFactory->recurring ?>" />
+	<?php } ?>
+	<?php if ( $passthrough != false ) { ?>
+		<input type="hidden" name="aec_passthrough" value="<?php echo $passthrough ?>" />
+	<?php } ?>
+	<button type="submit" class="button btn btn-success" id="confirmation"><i class="icon-ok icon-white"></i><?php echo JText::_('BUTTON_CONFIRM') ?></button>
 </div>
