deda0d1a8ccd8abe8ce5dd104be578c2c0c12761
David Deutsch <skore@valanx.org>
2012-08-24 03:05:19 +0200

    mi error styling missing

--- newsrc/code/components/com_acctexp/tmpl/etacarinae/confirmation/tmpl/mierror.php
+++ newsrc/code/components/com_acctexp/tmpl/etacarinae/confirmation/tmpl/mierror.php
@@ -11,7 +11,7 @@
 // Dont allow direct linking
 ( defined('_JEXEC') || defined( '_VALID_MOS' ) ) or die( 'Direct Access to this location is not allowed.' ) ?>
 <?php if ( !empty( $InvoiceFactory->mi_error ) ) { ?>
-	<div id="alert alert-error">
+	<div class="alert alert-error">
 		<?php foreach ( $InvoiceFactory->mi_error as $error ) {
 			echo '<p>' . $error . '</p>';
 		} ?>
