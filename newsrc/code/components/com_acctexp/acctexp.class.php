b4bec4cb29ee66b6c367b7089385ca750811db4d
David Deutsch <skore@valanx.org>
2012-10-19 23:07:45 +0200

    fixes #158

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -16860,6 +16860,22 @@ class AECToolbox
 					);
 	}
 
+	function getIETF_lang()
+	{
+		return array(	'ab','aa','af','ak','sq','am','ar','an','hy','as','av','ae','ay','az','bm','ba','eu',
+						'be','bn','bh','bi','bs','br','bg','my','ca','ch','ce','ny','zh','cv','kw','co','cr',
+						'hr','cs','da','dv','nl','dz','en','eo','et','ee','fo','fj','fi','fr','ff','gl','ka',
+						'de','el','gn','gu','ht','ha','he','hz','hi','ho','hu','ia','id','ie','ga','ig','ik',
+						'io','is','it','iu','ja','jv','kl','kn','kr','ks','kk','km','ki','rw','ky','kv','kg',
+						'ko','ku','kj','la','lb','lg','li','ln','lo','lt','lu','lv','gv','mk','mg','ms','ml',
+						'mt','mi','mr','mh','mn','na','nv','nb','nd','ne','ng','nn','no','ii','nr','oc','oj',
+						'cu','om','or','os','pa','pi','fa','pl','ps','pt','qu','rm','rn','ro','ru','sa','sc',
+						'sd','se','sm','sg','sr','gd','sn','si','sk','sl','so','st','es','su','sw','ss','sv',
+						'ta','te','tg','th','ti','bo','tk','tl','tn','to','tr','ts','tt','tw','ty','ug','uk',
+						'ur','uz','ve','vi','vo','wa','cy','wo','fy','xh','yi','yo','za','zu'
+					);
+	}
+
 	/**
 	 * get user ip & isp
 	 *
@@ -20955,8 +20971,10 @@ class Coupon extends serialParamDBTable
 
 	function decrementCount()
 	{
-		$this->usecount -= 1;
-		$this->storeload();
+		if ( $this->usecount ) {
+			$this->usecount -= 1;
+			$this->storeload();
+		}
 	}
 
 	function generateCouponCode( $maxlength = 6 )
