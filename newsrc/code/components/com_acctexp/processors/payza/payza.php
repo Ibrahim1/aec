fa521639aa34ef785de4a9cc005682dc44d39b7a
David Deutsch <skore@valanx.org>
2013-02-04 16:03:25 +0100

    re #242 getting rid of ap_test

--- newsrc/code/components/com_acctexp/processors/payza/payza.php
+++ newsrc/code/components/com_acctexp/processors/payza/payza.php
@@ -60,11 +60,8 @@ class processor_payza extends POSTprocessor
 
 	function createGatewayLink( $request )
 	{
-		
 		if ( $this->settings['testmode'] ) {
 			$var['post_url']	= "https://sandbox.payza.com/sandbox/payprocess.aspx";
-
-			$var['ap_test'] = '1';
 		} else {
 			$var['post_url']	= "https://secure.payza.com/checkout";
 		}
