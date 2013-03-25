bd07ddb92693cd654a6fd93b719e4e4446ae8657
David Deutsch <skore@valanx.org>
2013-03-25 13:04:28 +0100

    re #271 pretty much done, ready for testing

--- newsrc/code/components/com_acctexp/processors/cyberplus/cyberplus.php
+++ newsrc/code/components/com_acctexp/processors/cyberplus/cyberplus.php
@@ -20,8 +20,8 @@ class processor_cyberplus extends POSTprocessor
 		$info['longname']		= JText::_('CFG_CYBERPLUS_LONGNAME');
 		$info['statement']		= JText::_('CFG_CYBERPLUS_STATEMENT');
 		$info['description']	= JText::_('CFG_CYBERPLUS_DESCRIPTION');
-		$info['currencies']		= 'EUR';
-		$info['languages']		= "FR,EN,DE,IT,ES,NL";
+		$info['currencies']		= AECToolbox::aecCurrencyField( true, true, true, true );
+		$info['languages']		= "DE,EN,ZH,ES,FR,IT,JP";
 		$info['cc_list']		= "visa,mastercard,discover,americanexpress,echeck";
 		$info['recurring']		= 2;
 
@@ -31,13 +31,14 @@ class processor_cyberplus extends POSTprocessor
 	function settings()
 	{
 		$settings = array();
-		$settings['testmode']		= 0;
-		$settings['key']			= '0123456789ABCDEF0123456789ABCDEF01234567';
-		$settings['currency']		= 'EUR';
-		$settings['language']		= 'FR';
-		$settings['server']			= 0;
-		$settings['item_name']		= sprintf( JText::_('CFG_PROCESSOR_ITEM_NAME_DEFAULT'), '[[cms_live_site]]', '[[user_name]]', '[[user_username]]' );
-		$settings['customparams']	= "";
+		$settings['testmode']			= 0;
+		$settings['key']				= '0123456789ABCDEF0123456789ABCDEF01234567';
+		$settings['currency']			= 'EUR';
+		$settings['language']			= 'FR';
+		$settings['server']				= 0;
+		$settings['totalOccurrences']	= 12;
+		$settings['item_name']			= sprintf( JText::_('CFG_PROCESSOR_ITEM_NAME_DEFAULT'), '[[cms_live_site]]', '[[user_name]]', '[[user_username]]' );
+		$settings['customparams']		= "";
 
 		$settings = AECToolbox::rewriteEngineInfo( null, $settings );
 
@@ -48,14 +49,14 @@ class processor_cyberplus extends POSTprocessor
 	function backend_settings()
 	{
 		$settings = array();
-		$settings['testmode']		= array( 'toggle' );
-		$settings['tpe']			= array( 'inputC' );
-		$settings['soc']			= array( 'inputC' );
-		$settings['pass']			= array( 'inputC' );
-		$settings['key']			= array( 'inputC' );
-		$settings['language']		= array( 'list_language' );
-		$settings['item_name']		= array( 'inputE' );
-		$settings['customparams']	= array( 'inputD' );
+		$settings['testmode']			= array( 'toggle' );
+		$settings['tpe']				= array( 'inputC' );
+		$settings['pass']				= array( 'inputC' );
+		$settings['key']				= array( 'inputC' );
+		$settings['language']			= array( 'list_language' );
+		$settings['totalOccurrences']	= array( 'inputA' );
+		$settings['item_name']			= array( 'inputE' );
+		$settings['customparams']		= array( 'inputD' );
 
 		return $settings;
 	}
@@ -67,16 +68,35 @@ class processor_cyberplus extends POSTprocessor
 		$var['vads_site_id']		= $this->settings['siteid'];
 		$var['vads_ctx_mode']		= 'PRODUCTION';
 		$var['vads_version']		= 'V2';
-		$var['vads_language']		= 'fr';
-		$var['vads_currency']		= '978';
-		$var['vads_amount']			= $request->int_var['amount']*100;
+		$var['vads_language']		= strtoupper( $this->settings['language'] );
+		$var['vads_currency']		= AECToolbox::aecNumCurrency( $this->settings['currency'] );
+
+		if ( is_array( $request->int_var['amount'] ) ) {
+			$var['vads_amount']			= $request->int_var['amount']['amount3']*100;
+		} else {
+			$var['vads_amount']			= $request->int_var['amount']*100;
+		}
+
 		$var['vads_page_action']	= 'PAYMENT';
 		$var['vads_action_mode']	= 'INTERACTIVE';
 		$var['vads_validation_mode']= '0';
 		$var['vads_payment_cards']	= '';
 
 		if ( is_array( $request->int_var['amount'] ) ) {
-			$var['vads_payment_config']	= 'MULTI:first=900;count=12;period=30';
+			$vars = array();
+			if ( isset( $request->int_var['amount']['amount1'] ) ) {
+				$vars['first'] = $request->int_var['amount']['amount1']*100;
+			}
+
+			$vars['count'] = $this->settings['totalOccurrences'];
+			$vars['period'] = $this->convertPeriodUnit( $request->int_var['amount']['unit3'], $request->int_var['amount']['period3'] );
+
+			$varr = array();
+			foreach ( $vars as $k => $v ) {
+				$varr[] = $k.'='.$v;
+			}
+
+			$var['vads_payment_config']	= 'MULTI:'.implode(';', $varr);
 		} else {
 			$var['vads_payment_config']	= 'SINGLE';
 		}
@@ -87,7 +107,6 @@ class processor_cyberplus extends POSTprocessor
 		$var['vads_cust_name']		= $request->metaUser->cmsUser->name;
 		$var['vads_cust_email']		= $request->metaUser->cmsUser->email;
 		$var['vads_cust_country']	= $this->settings['language'];
-		$var['societe']				= $this->settings['soc'];
 		$var['vads_return_mode']	= 'GET';
 		$var['vads_redirect_success_timeout'] 	= '5';
 		$var['vads_redirect_success_message'] 	= 'Redirection vers la boutique dans quelques instants';
@@ -97,162 +116,101 @@ class processor_cyberplus extends POSTprocessor
 		$var['vads_trans_id']		= str_pad($request->invoice->id, 6, "0", STR_PAD_LEFT);
 		$var['vads_trans_date']		= gmdate("YmdHis",time());
 		$var['vads_url_return']		= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=thanks" );
-		$var['url_refused']			= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=cancel" );
+		$var['vads_url_refused']	= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=cancel" );
 		$var['vads_url_cancel']		= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=cancel" );
-		$var['url_error']			= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=cancel" );
+		$var['vads_url_error']		= AECToolbox::deadsureURL( "index.php?option=com_acctexp&amp;task=cancel" );
 
 		$var['signature'] = $this->getHash( $var );
 
-		foreach ( $var as $k => $v ) {
-			$var[$k] = $this->HtmlEncode( $v );
+		return $var;
+	}
+
+	function convertPeriodUnit( $unit, $period )
+	{
+		$return = array();
+		switch ( $unit ) {
+			case 'D':
+				return $period;
+				break;
+			case 'W':
+				return $period*7;
+				break;
+			case 'M':
+				return $period*30;
+				break;
+			case 'Y':
+				return $period*365;
+				break;
 		}
 
-		return $var;
+		return $return;
 	}
 
 	function parseNotification( $post )
 	{
 		$response = array();
-		$response['invoice'] = $post['texte-libre'];
+		$response['invoice']			= $post['vads_order_id'];
+		$response['amount_paid']		= $post['vads_amount']/100;
+		$response['amount_currency']	= AECToolbox::aecNumCurrency( $post['vads_currency'], true );
 
 		return $response;
 	}
 
 	function validateNotification( $response, $post, $invoice )
 	{
-		switch( $post['retour'] ) {
-			case 'payetest':
-				$response['valid']	= $this->settings['testmode'] ? true : false;
-				break;
-			case 'paiement':
+		$response['valid'] = false;
+
+		if ( empty( $post['signature'] ) ) {
+			$response['error'] = true;
+			$response['errormsg'] = 'signature missing';
+
+			return $response;
+		}
+
+		if ( $post['signature'] !== $this->getHash( $post ) ) {
+			$response['error'] = true;
+			$response['errormsg'] = 'invalid signature';
+
+			return $response;
+		}
+
+		switch( $post['vads_result'] ) {
+			case '00':
 				$response['valid']	= true;
 				break;
-			case 'annulation':
-				$response['valid']	= false;
+			case '17':
 				$response['cancel']	= 1;
 				break;
+			default:
+				$response['error']	= true;
+				if ( !empty( $post['vads_extra_result'] ) ) {
+					$response['errormsg'] = $post['vads_result'].': '.$post['vads_extra_result'];
+				} else {
+					$response['errormsg'] = $post['vads_result'];
+				}
+				break;
 		}
-
-		if ( $post['MAC'] !== $this->getHash( $post, false ) ) {
-			$response['pending_reason'] = 'invalid HMAC';
-			$response['valid'] = false;
-		}
-
-		$response['amount_paid'] = substr( $response['montant'], -3 );
-		$response['amount_currency'] = str_replace( $response['amount_paid'], '', $response['montant'] );
 
 		return $response;
 	}
 
-	function HtmlEncode( $data )
+	function getHash( $array )
 	{
-		$SAFE_OUT_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890._-";
-		$encoded_data = "";
-		$result = "";
-		for ($i=0; $i<strlen($data); $i++) {
-			if (strchr($SAFE_OUT_CHARS, $data{$i})) {
-				$result .= $data{$i};
-			} else if (($var = bin2hex(substr($data,$i,1))) <= "7F") {
-				$result .= "&#x" . $var . ";";
-			} else {
-				$result .= $data{$i};
+		ksort( $array );
+
+		$glue = '+';
+
+		foreach ( $array as $k => $v ) {
+			if ( strpos( $k, 'vads_' ) === 0 ) {
+				$check[] = $v;
 			}
 		}
-		return $result;
-	}
 
-	function getHash( $array, $send=true )
-	{
-		if ( $send ) {
-			$array['lupper']	= strtoupper( $array['vads_language'] );
-			$array['key']		= $this->settings['tpe'];
-
-			$check = array();
-
-			$keys = array(	'vads_action_mode', 'vads_amount', 'vads_capture_delay', 'vads_ctx_mode',
-							'vads_currency', 'lupper', 'vads_cust_email', 'vads_cust_id',
-							'vads_cust_name', 'vads_language', 'vads_order_id', 'vads_page_action',
-							'vads_payment_cards', 'vads_payment_config', 'vads_redirect_error_message', 'vads_redirect_error_timeout',
-							'vads_redirect_success_message', 'vads_redirect_success_timeout', 'vads_return_mode', 'vads_site_id',
-							'vads_trans_date', 'vads_trans_id', 'vads_url_cancel', 'vads_url_return',
-							'vads_validation_mode', 'vads_version', 'vads_version', 'vads_code'
-						);
-
-			$glue = '+';
-		} else {
-			$array['version']	= '3.0';
-			$array['key']		= $this->settings['tpe'];
-
-			$check = array( $this->settings['tpe'] );
-
-			$keys = array(	'key', 'date', 'montant', 'reference', 'texte-libre', 'version',
-							'code-retour', 'cvx', 'vld', 'brand', 'status3ds', 'numauto',
-							'motifrefus', 'originecb', 'bincb', 'hpancb', 'ipclient', 'originetr',
-							'veres', 'pares'
-						);
-
-			$glue = '*';
-		}
-
-		foreach ( $keys as $key ) {
-			if ( isset( $array[$key] ) ) {
-				$check[] = $array[$key];
-			} else {
-				$check[] = '';
-			}
-		}
+		$check[] = $this->settings['tpe'];
 
 		$string = implode( $glue, $check );
 
-		if ( $send ) {
-			return sha1( $string );
-		} else {
-			return $this->CMIC_hmac( $string );
-		}
-	}
-
-	function CMCIC_hmac( $data="")
-	{
-		$k1 = pack( "H*", sha1( $this->settings['pass'] ) );
-		$l1 = strlen( $k1 );
-		$k2 = pack( "H*", $this->settings['key'] );
-		$l2 = strlen( $k2 );
-
-		if ( $l1 > $l2 ) {
-			//$k2 = str_pad( $k2, $l1, chr(0x00) );
-		} elseif ( $l2 > $l1 ) {
-			$k1 = str_pad( $k1, $l2, chr(0x00) );
-		}
-
-		if ( $data == "" ) {
-			$d = "CtlHmac" . $this->settings['ver'] . $this->settings['tpe'];
-		} else {
-			$d = $data;
-		}
-
-		return strtolower( $this->hmac( $k2, $d ) );
-		// return strtolower( $this->hmac( $k1 ^ $k2, $d ) );
-	}
-
-	function hmac( $key, $data )
-	{
-		// RFC 2104 HMAC implementation for php.
-		// Creates an md5 HMAC.
-		// Eliminates the need to install mhash to compute a HMAC
-		// Hacked by Lance Rushing
-
-		$b = 64; // byte length for md5
-
-		if ( strlen( $key ) > $b ) {
-			$key = pack( "H*", md5( $key ) );
-		}
-		$key  = str_pad( $key, $b, chr(0x00) );
-		$ipad = str_pad( '', $b, chr(0x36) );
-		$opad = str_pad( '', $b, chr(0x5c) );
-		$k_ipad = $key ^ $ipad;
-		$k_opad = $key ^ $opad;
-
-		return md5( $k_opad  . pack( "H*", md5( $k_ipad . $data ) ) );
+		return sha1( $string );
 	}
 
 }
