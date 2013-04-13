523da22d30f0c89d6896913e50b6ce57494c3f59
David Deutsch <skore@valanx.org>
2013-04-13 15:18:42 +0200

    well, now it is

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -14,7 +14,7 @@
 global $aecConfig;
 
 define( '_AEC_VERSION', '1.2RC' );
-define( '_AEC_REVISION', '6107' );
+define( '_AEC_REVISION', '6109' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
