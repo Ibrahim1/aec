3ec19d15b72a59187f65f61bdf8114d1ea26578a
David Deutsch <skore@valanx.org>
2013-04-13 13:34:18 +0200

    now biggie, just doing pretty much EVERYTHING automatically now

--- scripts/git/common.php
+++ scripts/git/common.php
@@ -20,3 +20,24 @@ function fsnr( $path, $search, $count )
 		return null;
 	}
 }
+
+function versionGet()
+{
+	// And HE told me the count of the git
+	$count = trim( shell_exec( 'git rev-list HEAD --count' ) );
+
+	// And on the thirteenth day of the fourth month of the year two thousand and thirteen,
+	// The hardware counter stood at six thousand one hundred and two,
+	// Yet THE COMPUTER spoke of four thousand eight hundred and ninety eight,
+	// Thus it was deemed that the difference shall be one thousand two hundred and four
+	// One Thousand two hundred and four shall be the number, no more, no less
+	// Not one thousand two hundred and three, nor one thousand two hundred and five
+
+	$count += 1204;
+
+	// And since we knoweth that we shall be incrementing the counter, we shall do so
+
+	$count++;
+
+	return $count;
+}
---
+++ scripts/git/post-commit.php
@@ -0,0 +1,3 @@
+<?php
+
+shell_exec( 'phing -f '.__DIR__.'/../build/build.xml' );
