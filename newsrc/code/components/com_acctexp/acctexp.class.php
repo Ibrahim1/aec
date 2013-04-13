0cb137274da89d50aa30d16668198f344577142f
David Deutsch <skore@valanx.org>
2013-04-13 12:19:56 +0200

    new branching stuff, testing pre-commit

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -14,7 +14,7 @@
 global $aecConfig;
 
 define( '_AEC_VERSION', '1.2RC' );
-define( '_AEC_REVISION', '6100' );
+define( '_AEC_REVISION', '6103' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
---
+++ scripts/git/pre-commit.php
@@ -0,0 +1,46 @@
+<?php
+
+// And HE told me the count of the git
+$count = trim( shell_exec( 'git rev-list HEAD --count' ) );
+
+// And on the thirteenth day of the fourth month of the year two thousand and thirteen,
+// The hardware counter stood at six thousand one hundred and two,
+// Yet THE COMPUTER spoke of four thousand eight hundred and ninety eight,
+// Thus it was deemed that the difference shall be one thousand two hundred and four
+// One Thousand two hundred and four shall be the number, no more, no less
+// Not one thousand two hundred and three, nor one thousand two hundred and five
+
+$count += 1204;
+
+// And since we knoweth that we shall be incrementing the counter, we shall do so
+
+$count++;
+
+// And changeth be the class file
+fsnr( __DIR__.'/../../newsrc/code/components/com_acctexp/acctexp.class.php', "define( '_AEC_REVISION', '", $count );
+
+// And changeth be the xml file
+fsnr( __DIR__.'/../build/build.xml', '<property name="git.lastrevision" value="', $count );
+
+file_put_contents( '/home/skore/test.txt', $count.' '.time()."\n", FILE_APPEND );
+
+function fsnr( $path, $search, $count )
+{
+	$content = file_get_contents( $path );
+
+	$start = strpos( $content, $search )+strlen($search);
+
+	$delim = substr( $search, -1, 1 );
+
+	$length = strpos( $content, $delim, $start ) - $start;
+
+	$old = substr( $content, $start, $length );
+
+	if ( $old != $count ) {
+		$content = str_replace( $search.$old.$delim, $search.$count.$delim, $content );
+
+		return file_put_contents( $path, $content );
+	} else {
+		return null;
+	}
+}
