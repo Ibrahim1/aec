bbf0e003310da6d6d4cf770bf4427d8b9e7fdc2f
David Deutsch <skore@valanx.org>
2013-05-20 10:59:23 +0200

    fixes #310

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 define( '_AEC_VERSION', '1.2RC' );
-define( '_AEC_REVISION', '6196' );
+define( '_AEC_REVISION', '6197' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
