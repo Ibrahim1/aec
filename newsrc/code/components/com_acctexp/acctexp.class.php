c170fd13496dbf8df86a3a843a671554521aa4b7
David Deutsch <skore@valanx.org>
2013-05-29 14:31:32 +0200

    triggering build

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -12,7 +12,8 @@
 defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 define( '_AEC_VERSION', '1.2RC' );
-define( '_AEC_REVISION', '6207' );
+
+define( '_AEC_REVISION', '6208' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
