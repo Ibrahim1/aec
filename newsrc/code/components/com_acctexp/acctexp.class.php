16663faad03b4dd0fa6a020907c5e9c466ab8c49
David Deutsch <skore@valanx.org>
2014-12-16 14:43:24 +0100

    style for explanation boxes on center page

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 define( '_AEC_VERSION', '1.2' );
-define( '_AEC_REVISION', '6467' );
+define( '_AEC_REVISION', '6468' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
