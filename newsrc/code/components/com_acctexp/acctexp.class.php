34e056b4fe4dcb12553594ba31e2d756cc3e2d5e
David Deutsch <skore@valanx.org>
2014-12-22 15:21:28 +0100

    merging down bootstrap 3.1 feature to fix table row.success hover stuff

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 define( '_AEC_VERSION', '1.2' );
-define( '_AEC_REVISION', '6500' );
+define( '_AEC_REVISION', '6501' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
