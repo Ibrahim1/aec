cf1c1d132f83a806a34a2363bd15b90c45765cb8
David Deutsch <skore@valanx.org>
2015-01-04 12:02:35 +0100

    fixing navbar overlapping joomla3 navbar popovers

--- newsrc/code/components/com_acctexp/acctexp.class.php
+++ newsrc/code/components/com_acctexp/acctexp.class.php
@@ -12,7 +12,7 @@
 defined('_JEXEC') or die( 'Direct Access to this location is not allowed.' );
 
 define( '_AEC_VERSION', '1.2' );
-define( '_AEC_REVISION', '6535' );
+define( '_AEC_REVISION', '6536' );
 
 include_once( JPATH_SITE . '/components/com_acctexp/lib/compat.php' );
 
