092144004f206adb4b1f90baab4cbf5fc5a642b3
David Deutsch <skore@valanx.org>
2015-03-11 00:26:01 +0100

    fixing up services edit layout

--- newsrc/code/administrator/components/com_acctexp/admin.acctexp.html.php
+++ newsrc/code/administrator/components/com_acctexp/admin.acctexp.html.php
@@ -3954,7 +3954,7 @@ jQuery(document).ready(function(jQuery) {
 		$tabs->newTab( 'mi', JText::_('AEC_SERVICE_TITLE') );
 
 		if ( $aecHTML->hasSettings ) {
-			$tabs->newTab( 'settings', JText::_('AEC_SERVICE_SETTINGS') );
+			//$tabs->newTab( 'settings', JText::_('AEC_SERVICE_SETTINGS') );
 		}
 
 		if ( !empty( $aecHTML->customparams ) ) {
@@ -3993,11 +3993,7 @@ jQuery(document).ready(function(jQuery) {
 			</div>
 			<div class="col-sm-6">
 				<section class="paper">
-					<?php if ( $aecHTML->hasSettings ) {
-						$tabs->nextPane( 'settings' ); ?>
-						<div class="row">
-						<div class="col-sm-8 col-sm-offset-2">
-						<section class="paper">
+					<?php if ( $aecHTML->hasSettings ) { ?>
 						<h4><?php echo JText::_('AEC_SERVICE_SETTINGS'); ?></h4>
 						<?php foreach ( $aecHTML->customparams as $name ) { ?>
 							<?php if ( strpos( $name, 'aectab_' ) === 0 ) { ?>
@@ -4011,9 +4007,6 @@ jQuery(document).ready(function(jQuery) {
 								echo $aecHTML->createSettingsParticle( $name );
 							} ?>
 						<?php } ?>
-						</section>
-						</div>
-						</div>
 					<?php } else { ?>
 
 					<?php } ?>
