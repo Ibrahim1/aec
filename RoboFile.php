5bc9463afbeb9acb132df3c63eed19e783d7eb7f
David Deutsch <skore@valanx.org>
2015-06-09 07:11:13 +0200

    more work converting to Robo Build

--- RoboFile.php
+++ RoboFile.php
@@ -2,3 +2,61 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+class RoboFile extends Robo\Tasks
+{
+	public function release()
+	{
+
+	}
+
+	public function versionBumpClass()
+	{
+		$this->replaceInFile(
+			__DIR__ . '/newsrc/code/components/com_acctexp/acctexp.class.php',
+			"define( '_AEC_REVISION', '",
+			$this->revisionGet()
+		);
+	}
+
+	public function replaceInFile( $path, $search, $count )
+	{
+		$content = file_get_contents( $path );
+
+		$start = strpos( $content, $search ) + strlen($search);
+
+		$delim = substr( $search, -1, 1 );
+
+		$length = strpos( $content, $delim, $start ) - $start;
+
+		$old = substr( $content, $start, $length );
+
+		if ( $old != $count ) {
+			$content = str_replace(
+				$search . $old . $delim,
+				$search . $count . $delim,
+				$content
+			);
+
+			return file_put_contents( $path, $content );
+		} else {
+			return null;
+		}
+	}
+
+	public function revisionGet()
+	{
+		// And HE told me the count of the git
+		$count = trim( $this->taskExec('git rev-list HEAD --count') )->run();
+
+		// And on the thirteenth day of the fourth month of the year two thousand and thirteen,
+		// The hardware counter stood at six thousand one hundred and two,
+		// Yet THE COMPUTER spoke of four thousand eight hundred and ninety eight,
+		// Thus it was deemed that the difference shall be one thousand two hundred and four
+		// One Thousand two hundred and four shall be the number, no more, no less
+		// Not one thousand two hundred and three, nor one thousand two hundred and five
+
+		$count += 1204;
+
+		return $count;
+	}
+}
